53b754f4254369561286d26f99188bc78059850a
bagus@paket.id <bagus@paket.id>
2020-12-07 14:33:06 +0700

    add inventor bagging

--- src/config.js
+++ src/config.js
@@ -47,6 +47,7 @@ export default {
     // transaction
     tariff_shipping_service: api + "tariff-shipping-service",
     connote: api + "connote",
-    koli: api + "koli"
+    koli: api + "koli",
+    bag: api + "bag"
 
 }
