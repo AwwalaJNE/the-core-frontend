eea01a6faa025782b46632ee18384aa3e7987123
donaderoyan <donaderoyan@gmail.com>
2021-01-06 09:43:42 +0700

    update surcharge

--- src/mixins/transaction.js
+++ src/mixins/transaction.js
@@ -345,44 +345,65 @@ const TransactionMixin = {
                     let tempbiaya = 0
                     let temp_handling_charge = 0
                     let temp_chargeable_weight = 0
+                    let reCompare = false
 
                     if(koli.surcharge_id && koli.surcharge_id.length > 0) {
                         
-                        let CHARGEBLE_WEIGHT = this.SUM_CHARGEBLE_WEIGHT
-                        let ACTUAL_WEIGHT = koli_actual_weight
-                        let BASE_TARIFF = this.BASE_TARIFF 
+                        let chargeble_weight = this.SUM_CHARGEBLE_WEIGHT
+                        let base_tariff = this.BASE_TARIFF 
+                        console.log('base_tariff', this.BASE_TARIFF )
                         let temp_actual = 0
                         koli.surcharge_id.map(su_id => {
                             let dataSurcharge = surchargeByID[su_id] || {}
                             
                             if(Object.keys(dataSurcharge).length > 0) {
                                 if(dataSurcharge.hasOwnProperty('surcharge_formula')) {
-                                    // dataSurcharge['surcharge_formula'].map(formula => {
-                                    //     let fmr = dataSurcharge['surcharge_formula'][formula].toLowerCase()
-                                    //     let str = ``
-                                    // })
-                                    if(dataSurcharge['surcharge_formula'].hasOwnProperty('SURCHARGE')) {
-                                        let evalSurcharge = eval(dataSurcharge['surcharge_formula']['SURCHARGE'])
+                                    Object.keys(dataSurcharge['surcharge_formula']).map(formula => {
+                                        if(dataSurcharge['surcharge_formula'].hasOwnProperty(formula)) {
+                                            if(formula.toLowerCase() == 'chargeble_weight') {
+                                                let str = isNaN(dataSurcharge['surcharge_formula'][formula]) ? dataSurcharge['surcharge_formula'][formula].toLowerCase() : dataSurcharge['surcharge_formula'][formula]
+                                                let evalchargeable_weight = eval(str)
+                                                temp_chargeable_weight = evalchargeable_weight
+                                                this.SUM_CHARGEBLE_WEIGHT = temp_chargeable_weight
+                                                this.BASE_TARIFF = tarifData.tarif * this.SUM_CHARGEBLE_WEIGHT
+                                                base_tariff = this.BASE_TARIFF
+                                                console.log('CHARGEBLE_WEIGHT', evalchargeable_weight, this.SUM_CHARGEBLE_WEIGHT, base_tariff)
+                                            } else if (formula.toLowerCase() == 'surcharge') {
+                                                let evalSurcharge = eval(dataSurcharge['surcharge_formula'][formula].toLowerCase())
+                                                // tempbiaya = Number(evalSurcharge)
+                                                tempbiaya = tempbiaya + Number(evalSurcharge)
+                                            } else if (formula.toLowerCase() == 'handling_charge') {
+                                                temp_handling_charge = Number(dataSurcharge['surcharge_formula'][formula])
+                                            } else if(formula.toLowerCase() == 'koli_actual_weight') {
+                                                let evalactual_weight = eval(dataSurcharge['surcharge_formula'][formula].toLowerCase())
+                                                koli_actual_weight = evalactual_weight
+                                                reCompare = true
+                                                console.log('ACTUAL_WEIGHT', evalactual_weight)
+                                            }
+                                        }
+                                    })
+                                    // if(dataSurcharge['surcharge_formula'].hasOwnProperty('SURCHARGE')) {
+                                    //     let evalSurcharge = eval(dataSurcharge['surcharge_formula']['SURCHARGE'])
                                         
-                                        tempbiaya = tempbiaya + Number(evalSurcharge)
-                                    }
-                                    if(dataSurcharge['surcharge_formula'].hasOwnProperty('HANDLING_CHARGE')) {
-                                        temp_handling_charge = Number(dataSurcharge['surcharge_formula']['HANDLING_CHARGE'])
+                                    //     tempbiaya = tempbiaya + Number(evalSurcharge)
+                                    // }
+                                    // if(dataSurcharge['surcharge_formula'].hasOwnProperty('HANDLING_CHARGE')) {
+                                    //     temp_handling_charge = Number(dataSurcharge['surcharge_formula']['HANDLING_CHARGE'])
                                         
-                                    }
-                                    if(dataSurcharge['surcharge_formula'].hasOwnProperty('CHARGEBLE_WEIGHT')) {
-                                        let evalchargeable_weight = eval(dataSurcharge['surcharge_formula']['CHARGEBLE_WEIGHT'])
-                                        this.SUM_CHARGEBLE_WEIGHT = evalchargeable_weight
-                                        console.log('CHARGEBLE_WEIGHT', evalchargeable_weight)
+                                    // }
+                                    // if(dataSurcharge['surcharge_formula'].hasOwnProperty('CHARGEBLE_WEIGHT')) {
+                                    //     let evalchargeable_weight = eval(dataSurcharge['surcharge_formula']['CHARGEBLE_WEIGHT'])
+                                    //     this.SUM_CHARGEBLE_WEIGHT = evalchargeable_weight
+                                    //     console.log('CHARGEBLE_WEIGHT', evalchargeable_weight)
                                         
-                                        // temp_chargeable_weight = temp_chargeable_weight + Number(evalchargeable_weight)
-                                    }
-                                    if(dataSurcharge['surcharge_formula'].hasOwnProperty('KOLI_ACTUAL_WEIGHT')) {
-                                        let evalactual_weight = eval(dataSurcharge['surcharge_formula']['KOLI_ACTUAL_WEIGHT'].toLowerCase())
-                                        koli_actual_weight = evalactual_weight
-                                        console.log('ACTUAL_WEIGHT', evalactual_weight)
-                                        // temp_chargeable_weight = temp_chargeable_weight + Number(evalchargeable_weight)
-                                    }
+                                    //     // temp_chargeable_weight = temp_chargeable_weight + Number(evalchargeable_weight)
+                                    // }
+                                    // if(dataSurcharge['surcharge_formula'].hasOwnProperty('KOLI_ACTUAL_WEIGHT')) {
+                                    //     let evalactual_weight = eval(dataSurcharge['surcharge_formula']['KOLI_ACTUAL_WEIGHT'].toLowerCase())
+                                    //     koli_actual_weight = evalactual_weight
+                                    //     console.log('ACTUAL_WEIGHT', evalactual_weight)
+                                    //     // temp_chargeable_weight = temp_chargeable_weight + Number(evalchargeable_weight)
+                                    // }
                                 }
                             }
                         })
@@ -394,8 +415,14 @@ const TransactionMixin = {
                     SUM_HANDLING_CHARGE = SUM_HANDLING_CHARGE + temp_handling_charge
 
                     // let reroundUp = Number(this.round03(this.SUM_VOLUME_WEIGHT))
-                    let reCompare = Number(Math.max(this.SUM_ACTUAL_WEIGHT, roundUp).toFixed(2))
-                    this.SUM_CHARGEBLE_WEIGHT = reCompare
+                    let reCompareWeight = Number(Math.max(this.SUM_ACTUAL_WEIGHT, roundUp).toFixed(2))
+                    this.SUM_CHARGEBLE_WEIGHT = reCompareWeight
+                    if(temp_chargeable_weight !== 0) {
+                        this.SUM_CHARGEBLE_WEIGHT += temp_chargeable_weight
+                    }
+                    // this.SUM_CHARGEBLE_WEIGHT += chargeble_weight_intervensi
+                    
+                    
 
                 })
 
