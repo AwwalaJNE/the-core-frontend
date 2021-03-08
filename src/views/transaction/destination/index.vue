e9924ba61168f2cf861e0cc70fcb2e23f26e6322
donaderoyan <donaderoyan@gmail.com>
2021-03-08 19:08:11 +0700

    fix tariff

--- src/views/transaction/destination/index.vue
+++ src/views/transaction/destination/index.vue
@@ -37,7 +37,7 @@
                         @formData="formData"
                         :dataItem="listenDataItem"
                         @searchTariffCode="searchTariffCode"
-                        @onChangeCustom="onChangeCustom"
+                        
                         typeForm="destination"
                     />
                 </transition>
@@ -117,21 +117,21 @@ export default {
         closeGetCustomer() {
             this.dialogGetCustomer = false
         },
-        onChangeCustom(key,val) {
-            if(key != undefined) {
-                if(key == 'destination_onchange_address') {
-                    this.$store.dispatch('SET_CALC_COMPONENT_SWITCH', true)
-                }
-            }
-        },
+        // onChangeCustom(key,val) {
+        //     if(key != undefined) {
+        //         if(key == 'destination_onchange_address') {
+        //             this.$store.dispatch('SET_CALC_COMPONENT_SWITCH', true)
+        //         }
+        //     }
+        // },
         updateValue(key,value,fromBooking = false) {
             if(Object.keys(value).length > 0 && key == 'detination') {
-                this.forcererender = true
+                // this.forcererender = true
                 let typeaddress = value.customer_address_type.toLowerCase() || ''
                 let zipndestiCode = {'zip_code' : value.geolocation_subdistrict_zip_code, 'destination_code': value.geolocation_subdistrict_tarif_code}
                 this.destinationCode = value.geolocation_subdistrict_tarif_code
                 let booking_connote_service_code = value.booking_connote_service_code ? value.booking_connote_service_code : ''
-                this.getShippingService(booking_connote_service_code, fromBooking)
+                
 
                 this.$store.dispatch(`SET_DESTINATION_DESTINATION_TYPE`, typeaddress)
                 this.$store.dispatch(`SET_DESTINATION_DESTINATION_NAME`, value.customer_name)
@@ -141,8 +141,14 @@ export default {
                 this.$store.dispatch(`SET_DESTINATION_DESTINATION_ONCHANGE_ADDRESS`, value.geolocation_location_name)
                 this.$store.dispatch(`SET_DESTINATION_DESTINATION_ZIP_CODE`, zipndestiCode)
 
-                let self = this
-                setTimeout(function(){ self.forcererender = false }, 100);
+                // let self = this
+                // this.$nextTick(() => {
+                //     this.getShippingService(booking_connote_service_code, fromBooking)
+                // });
+                // setTimeout(function(){ 
+                //     // self.forcererender = false 
+                //     // self.getShippingService(booking_connote_service_code, fromBooking)
+                // }, 3000);
             }
         },
         async getShippingService(booking_connote_service_code, fromBooking = false) {
@@ -169,6 +175,7 @@ export default {
                     this.$store.dispatch("SET_PACKAGE_PACKAGE_SERVICE_arrData", arr.length > 0 ? arr : [])
 
                     if(fromBooking == true && booking_connote_service_code != '') {
+                        console.log('jangan jalanin lagi')
                         let dat = arr.filter(item => item.value == booking_connote_service_code)
                         let serviceItem = dat[0]
                         this.$store.dispatch("SET_PACKAGE_PACKAGE_SERVICE_ValueData", serviceItem)
