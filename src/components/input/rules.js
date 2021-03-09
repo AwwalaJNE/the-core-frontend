8e13d62f06c372d786e7e1a276a071f83c92e611
donaderoyan <donaderoyan@gmail.com>
2021-03-09 16:30:29 +0700

    fix phone regex

--- src/components/input/rules.js
+++ src/components/input/rules.js
@@ -7,7 +7,8 @@ const phoneRule = {
   },
   validate(value, args) {
     //   /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/g;
-    const MOBILEREG = /^(^\+62|62|^0)(\d{3,4}-?){2}\d{3,4}$/g;
+    // /^(^\+62|62|^0)(\d{3,4}-?){2}\d{3,4}$/g
+    const MOBILEREG = /^[\+]?(\d{7,16})$/;
 
     return MOBILEREG.test(value);
   }
