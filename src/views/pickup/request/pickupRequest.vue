f194275097309bf7fdc8d58cf4db5ede0fe8ff3b
bagus@paket.id <bagus@paket.id>
2021-03-16 17:55:39 +0700

    fix origin destination

--- src/views/pickup/request/pickupRequest.vue
+++ src/views/pickup/request/pickupRequest.vue
@@ -103,7 +103,7 @@ export default {
             endDate: "",
             dialogTariff: false,
             pagination: {
-                limit:5,
+                limit:20,
                 page_size: 1,
                 page: 1
             },
