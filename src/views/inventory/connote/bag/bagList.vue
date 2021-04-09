770987ec55f71f77a72d6fe80e3381b7311bab27
Rizlan Tamima <rizlantamima@Rizlans-MacBook-Pro.local>
2021-04-09 20:51:26 +0700

    fix pirnt bag

--- src/views/inventory/connote/bag/bagList.vue
+++ src/views/inventory/connote/bag/bagList.vue
@@ -153,7 +153,8 @@ export default {
                 name: 'printGeneral', 
                 params: { 
                     'id': val.bag_number, 
-                    'type': 'bag'
+                    'type': 'bag',
+                    'node_id': this.listenNodeId
                 } 
             });
         window.open(routeData.href, '_blank');
