9b3ead40087b6acba2fee0bfac337efc653dc217
Rizlan Tamima <rizlantamima@Rizlans-MacBook-Pro.local>
2021-04-09 18:13:46 +0700

    fix bag detail info

--- src/views/inventory/bag/bagDetailList.vue
+++ src/views/inventory/bag/bagDetailList.vue
@@ -178,7 +178,7 @@ export default {
           this.bag_detail_qty = val.data.data.bag_detail_qty
           this.total_connote = val.data.detail.length
           this.total_weight = val.data.data.bag_weight
-          this.actual_weight = val.data.data.bag_weight
+          this.actual_weight = val.data.data.bag_actual_weight
           this.bag_destination = val.data.data.destination ? val.data.data.destination.node_code : ''
         },
         actionUpdate(val){
