f3964fb17e748afb73d4ebbc605310d7c130ebab
Rizlan Tamima <rizlantamima@Rizlans-MacBook-Pro.local>
2021-04-09 20:01:42 +0700

    fix lagi

--- src/views/report/cashless.vue
+++ src/views/report/cashless.vue
@@ -190,7 +190,7 @@ export default {
                     this.keysData.push(obj)
                     this.handleClearForm()
                     this.closeLoading()
-                    this.openNotification('danger', 'Failed!', err.response.data.message)
+                    this.openNotification('danger', 'Failed!', (err.response.data.message ?? 'System Err').toUpperCase())
                 })
         },
         handleClearForm(){
