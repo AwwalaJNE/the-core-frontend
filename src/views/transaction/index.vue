8b1f46c4e3630cbfd3c1a5bbbd3da8470d8ef542
donaderoyan <donaderoyan@gmail.com>
2021-06-21 13:25:17 +0700

    fix focus setelah pke booking code

--- src/views/transaction/index.vue
+++ src/views/transaction/index.vue
@@ -239,6 +239,9 @@ export default {
                 this.$store.dispatch(`SET_PACKAGE_PACKAGE_COD_Visible`, false)
                 this.$store.dispatch(`SET_PACKAGE_PACKAGE_COD`, 0)
             }
+            this.$nextTick(() => {
+                this.$refs.originComponent.setFocus() 
+            });
             this.hasCodeBooking = false
             this.bookingCode = ""
         },
@@ -281,6 +284,10 @@ export default {
                     destinationObj['booking_connote_service_code'] = data.booking_connote_service_code
                     this.$refs.destinationComponent.updateValue('detination', destinationObj, true)
                     this.$store.dispatch(`SET_DESTINATION_DESTINATION_ADDRESS`, data.booking_connote_receiver_street_address)
+
+                    this.$nextTick(() => {
+                       this.$refs.originComponent.setFocus() 
+                    });
                 } else {
                     this.openNotification('danger', 'Booking code not found', err.response ? err.response.data.message : 'something went wrong')
                 }
