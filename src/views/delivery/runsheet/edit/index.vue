5996233ee1d2bef5ea9d2357122bf8302901436a
bagus@paket.id <bagus@paket.id>
2021-08-19 11:57:59 +0700

    fix delivery courier

--- src/views/delivery/runsheet/edit/index.vue
+++ src/views/delivery/runsheet/edit/index.vue
@@ -14,13 +14,13 @@
         <vs-col lg="12" sm="12" xs="12">
           <div class="box information" style="padding-top: 1px !important">
             <p align="left"><b>Courier</b></p>
-            <template v-if="dataDelivery">
-
+            <template v-if="dataDelivery || employee_data">
               <p align="left">
-                {{ dataDelivery[0] ? dataDelivery[0].employee_code : null }}
-                ({{dataDelivery[0] ? dataDelivery[0].employee_name : '-' }})
+                {{ dataDelivery[0] ? dataDelivery[0].employee_code : employee_data.employee_code }}
+                ({{dataDelivery[0] ? dataDelivery[0].employee_name : employee_data.employee_name }})
               </p>
             </template>
+
 
             <div class="nav-box">
               <vs-row>
@@ -136,6 +136,7 @@ export default {
       form: {},
       delivery_runsheet_number: "",
       employee_id: "",
+      employee_data: {},
       dataDelivery: [],
       summary: [],
       arrStatus: null,
@@ -175,9 +176,14 @@ export default {
     },
     getParamRoute() {
       this.employee_id = this.$route.params.employee_id.toString();
+
+      this.employee_data.employee_name = this.$route.params.employee_name
+      this.employee_data.employee_code = this.$route.params.employee_code
+
       if (this.$route.name == "delivery-runsheet-edit") {
         this.delivery_runsheet_number = this.$route.params.delivery_runsheet_number.toString();
         this.getDataDelivery();
+
       }
     },
     async scanConnote() {
