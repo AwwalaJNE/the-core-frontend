2cc8d92cb996694f2648fdc37ff4cd1d6618c819
donaderoyan <donaderoyan@gmail.com>
2021-09-01 14:51:11 +0700

    fix edit surcharge

--- src/views/settings/surcharge/surchargeList/dialogCreateEditSurcharge.vue
+++ src/views/settings/surcharge/surchargeList/dialogCreateEditSurcharge.vue
@@ -108,7 +108,7 @@ export default {
             
             // this.form['surcharge_condition'] = this.form['surcharge_condition'].toString()
             // this.form['surcharge_formula'] = this.form['surcharge_formula'].toString()
-            console.log("this.form", this.form)
+            // console.log("this.form", this.form)
             if(this.surcharge_id !== undefined && this.surcharge_id !== '') {
                     this.updateData()
             } else {
