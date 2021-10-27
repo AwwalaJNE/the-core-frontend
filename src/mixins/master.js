99e6a437f321236a5967e65089188647eb562782
fadholifh <fadholifh14@gmail.com>
2021-10-27 16:23:05 +0700

    Update danger notification
    Update danger notification

--- src/mixins/master.js
+++ src/mixins/master.js
@@ -72,8 +72,9 @@ const Master = {
         },
         openNotification(type = null, title,msg) {
             // type success, danger, warn
+            console.log("typenotif", type)
             const noti = this.$vs.notification({
-                duration: 6000,
+                duration: type == 'danger' ? 15000 : 6000,
                 progress: 'auto',
                 color: type,
                 position: 'top-right',
