b0fc8664ef46fa4822ae69e98071eb5c4f0c5e13
Sarah Fahrurrozi <sarahfahrurrozii@gmail.com>
2023-10-30 15:37:53 +0700

    fix bug

--- src/views/pickup/list/pickupList.vue
+++ src/views/pickup/list/pickupList.vue
@@ -277,7 +277,7 @@ export default {
                           item.total_weight = "";
                         }
                         item["isDisabled"] = (item.pickup_status == 'PICKED' || item.pickup_status == 'CANCELED' || item.pickup_status == 'DONE') ? true : false
-                        console.log(item, item.pickup_detail[0].total_weight, item.total_picked_weight, 'data');
+                        // console.log(item, item.total_weight, item.total_picked_weight, 'data');
                     })
                     this.dataTable = arr
                     this.pagination.page = res.data.meta.current_page
