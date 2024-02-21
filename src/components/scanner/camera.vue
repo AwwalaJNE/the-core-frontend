60594df28106968db399994a525c68688a7a6eb7
Fathurrohman <fathurrohmanrosyadi@gmail.com>
2024-02-21 16:22:29 +0700

    feat: web barcode scanner (test)

---
+++ src/components/scanner/camera.vue
@@ -0,0 +1,264 @@
+<template>
+  <div>
+    <vs-dialog
+      v-model="dialog"
+      class="dialog"
+      prevent-close
+      :loading="isLoading"
+      @close="handleClose"
+    >
+      <template #header>
+        <h4 class="not-margin"><b>Barcode Scanner</b></h4>
+      </template>
+
+      <div class="scanner-container">
+        <div v-show="!isLoading">
+          <video poster="data:image/gif,AAAA" ref="scanner"></video>
+          <div class="overlay-element"></div>
+          <div class="laser"></div>
+        </div>
+      </div>
+
+      <template #footer>
+        <div>
+          <el-select
+            placeholder="Camera"
+            v-model="cameraSource"
+            size="default"
+            class="select-devices"
+            @change="loadScanner"
+          >
+            <el-option
+              v-for="camera in availableCameras"
+              :key="camera.deviceId"
+              :label="camera.label"
+              :value="camera.deviceId"
+            >
+              {{ camera.label }}
+            </el-option>
+          </el-select>
+        </div>
+      </template>
+    </vs-dialog>
+  </div>
+</template>
+
+<script>
+import {
+  BrowserMultiFormatReader,
+  NotFoundException,
+  Exception,
+} from "@zxing/library";
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      isLoading: true,
+      codeReader: new BrowserMultiFormatReader(),
+      isMediaStreamAPISupported:
+        // eslint-disable-next-line operator-linebreak
+        navigator &&
+        // eslint-disable-next-line operator-linebreak
+        navigator.mediaDevices &&
+        "enumerateDevices" in navigator.mediaDevices,
+      cameraSource: "",
+      scannerRunning: false,
+      availableCameras: [],
+      eventNameSpace: null,
+    };
+  },
+
+  watch: {},
+
+  mounted() {
+    if (!this.isMediaStreamAPISupported) {
+      throw new Exception("Media Stream API is not supported");
+    }
+  },
+
+  beforeUnmount() {
+    if (this.scannerRunning) {
+      this.codeReader.reset();
+    }
+  },
+
+  methods: {
+    async getAvailableDevices() {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(
+          (device) => device.kind === "videoinput"
+        );
+
+        this.availableCameras = cameras.map((camera) => ({
+          deviceId: camera.deviceId,
+          label: camera.label || `Camera ${cameras.indexOf(camera) + 1}`,
+        }));
+
+        if (this.availableCameras.length > 0) {
+          this.cameraSource = this.availableCameras[0].deviceId;
+        }
+      } catch (error) {
+        this.infoError(error);
+      }
+    },
+
+    async open(eventNameSpace) {
+      this.dialog = true;
+      this.eventNameSpace = eventNameSpace;
+
+      await this.getAvailableDevices();
+
+      this.loadScanner();
+    },
+
+    scan(cameraSource) {
+      this.codeReader.decodeFromVideoDevice(
+        cameraSource,
+        this.$refs.scanner,
+        (result, err) => {
+          if (result) {
+            // this.$emit("decode", result.text);
+            // this.$emit("result", result);
+            console.log(result);
+            this.emitEvent("result", result);
+            this.handleClose();
+          }
+
+          if (err && !(err instanceof NotFoundException)) {
+            this.infoError(err);
+          }
+        }
+      );
+
+      this.scannerRunning = true;
+    },
+
+    loadScanner() {
+      this.isLoading = true;
+      if (this.scannerRunning) {
+        this.codeReader.reset();
+      }
+
+      this.scan(this.cameraSource);
+      this.$refs.scanner.oncanplay = (event) => {
+        this.isLoading = false;
+        this.$emit("loaded");
+      };
+    },
+
+    handleClose() {
+      if (this.scannerRunning) {
+        this.codeReader.reset();
+      }
+
+      this.scannerRunning = false;
+      this.dialog = false;
+      this.isLoading = true;
+    },
+
+    infoError(message) {
+      this.emitEvent("error", message);
+    },
+
+    emitEvent(event, data) {
+      this.$emit("data", {
+        event,
+        namespace: this.eventNameSpace,
+        data,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dialog >>> .vs-dialog {
+  max-width: 400px !important;
+  min-width: 400px !important;
+  width: 100%;
+}
+
+.barcode-scanner {
+  border: 1px #6c7784 solid;
+  padding: 0px;
+  margin-bottom: 10px;
+  height: 200px;
+  width: 100%;
+  overflow: hidden;
+}
+
+.select-devices.el-select {
+  border-radius: none !important;
+  border: none !important;
+}
+
+video {
+  max-width: 100%;
+  max-height: 100%;
+}
+.scanner-container {
+  position: relative;
+}
+
+.overlay-element {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 99%;
+  background: rgba(30, 30, 30, 0.5);
+
+  -webkit-clip-path: polygon(
+    0% 0%,
+    0% 100%,
+    20% 100%,
+    20% 20%,
+    80% 20%,
+    80% 80%,
+    20% 80%,
+    20% 100%,
+    100% 100%,
+    100% 0%
+  );
+  clip-path: polygon(
+    0% 0%,
+    0% 100%,
+    20% 100%,
+    20% 20%,
+    80% 20%,
+    80% 80%,
+    20% 80%,
+    20% 100%,
+    100% 100%,
+    100% 0%
+  );
+}
+
+.laser {
+  width: 60%;
+  margin-left: 20%;
+  background-color: tomato;
+  height: 1px;
+  position: absolute;
+  top: 40%;
+  z-index: 2;
+  box-shadow: 0 0 4px red;
+  -webkit-animation: scanning 2s infinite;
+  animation: scanning 2s infinite;
+}
+
+@-webkit-keyframes scanning {
+  50% {
+    -webkit-transform: translateY(75px);
+    transform: translateY(75px);
+  }
+}
+
+@keyframes scanning {
+  50% {
+    -webkit-transform: translateY(75px);
+    transform: translateY(75px);
+  }
+}
+</style>
--- src/views/inventory/unbag/index.vue
+++ src/views/inventory/unbag/index.vue
@@ -1,135 +1,146 @@
 <template>
-    <div>
-        <vs-row justify="space-between">
-            <vs-col xs="6" sm="4" lg="4">
-                <div class="titlePage">
-                    <breadcrumb />
-                    <h2>{{title}}</h2>
-                </div>
-            </vs-col>
+  <div>
+    <vs-row justify="space-between">
+      <vs-col xs="6" sm="4" lg="4">
+        <div class="titlePage">
+          <breadcrumb />
+          <h2>{{ title }}</h2>
+        </div>
+      </vs-col>
+    </vs-row>
+    <template>
+      <div class="center in-get-bag">
+        <vs-row style="margin-top:1em">
+          <vs-col xs="12" sm="4" lg="2" style="margin-bottom: 10px;">
+            <vs-radio v-model="radio_option" val="connote">
+              Connote (Orion)
+            </vs-radio>
+          </vs-col>
+          <vs-col xs="12" sm="4" lg="2">
+            <vs-radio v-model="radio_option" val="koli">
+              Koli
+            </vs-radio>
+          </vs-col>
         </vs-row>
-        <template>
-          <div class="center in-get-bag">
-            <vs-row style="margin-top:1em">
-              <vs-col xs="12" sm="4" lg="2">
-                <vs-radio
-                  v-model="radio_option"
-                  val="connote">
-                  Connote (Orion)
-                </vs-radio>
-              </vs-col>
-              <vs-col xs="12" sm="4" lg="2">
-                <vs-radio
-                  v-model="radio_option"
-                  val="koli">
-                  Koli
-                </vs-radio>
-              </vs-col>
-            </vs-row>
-          </div>
-        </template>
-        <section class="bagging">
-            <vs-row>
-              <vs-col xs="2" sm="2" lg="2">
-                <template>
-                  <div class="center in-get-bag">
-                    <vs-input border type="text"
-                              v-model="item_code"
-                              :label-placeholder="inputLabelPlaceholder"
-                              :autofocus="true"
-                              @keyup.enter.native="updateValue"
-                              ref="formInputUnbagging">
-
-                    </vs-input>
-                  </div>
+      </div>
+    </template>
+    <section class="bagging">
+      <vs-row>
+        <vs-col xs="8" sm="4" lg="2">
+          <template>
+            <div class="center in-get-bag">
+              <vs-input
+                border
+                type="text"
+                v-model="item_code"
+                :label-placeholder="inputLabelPlaceholder"
+                :autofocus="true"
+                ref="formInputUnbagging"
+                icon-after
+                @keyup.enter.native="updateValue"
+                @click-icon="$refs.cameraScanner.open('formInputUnbagging')"
+              >
+                <template #icon>
+                  <i class="bx bx-barcode-reader"></i>
                 </template>
-              </vs-col>
-              <vs-col xs="1" sm="1" lg="1">
-                <template>
-                  <div class="center in-get-bag">
-                    <vs-button
-                        @click="updateValue"
-                    >Submit</vs-button>
-                  </div>
-                </template>
-              </vs-col>
-            </vs-row>
+              </vs-input>
+            </div>
+          </template>
+        </vs-col>
+        <vs-col xs="4" sm="2" lg="1">
+          <template>
+            <div class="center in-get-bag">
+              <vs-button @click="updateValue">Submit</vs-button>
+            </div>
+          </template>
+        </vs-col>
+      </vs-row>
 
-            <vs-row justify="space-between" class=" mt-2">
-              <unbagDetail ref="unbagDetail" :itemNumber="item_number" ></unbagDetail>
-            </vs-row>
-        </section>
+      <vs-row justify="space-between" class=" mt-2">
+        <unbagDetail ref="unbagDetail" :itemNumber="item_number"></unbagDetail>
+      </vs-row>
+    </section>
 
-    </div>
+    <camera-scanner ref="cameraScanner" @data="onCameraScannerGetData" />
+  </div>
 </template>
 <script>
 import axios from "axios";
-import master from "@/mixins/master"
-import Breadcrumb from "@/components/breadcrumb/index"
-import unbagDetail from "@/views/inventory/unbag/unbagDetailList"
-
+import master from "@/mixins/master";
+import Breadcrumb from "@/components/breadcrumb/index";
+import unbagDetail from "@/views/inventory/unbag/unbagDetailList";
+import CameraScanner from "@/components/scanner/camera.vue";
 
 export default {
-    name:"InventoryUnbagging",
-    mixins: [master],
-    components: {
-        "breadcrumb": Breadcrumb,
-        "unbagDetail":unbagDetail
-    },
-    data() {
-        return {
-            title: "Unbagging",
-            item_code:'',
-            item_number:'',
-            form:{},
-            inputLabelPlaceholder: "Masukan code BAG",
-            radio_option: 'connote'
-
+  name: "InventoryUnbagging",
+  mixins: [master],
+  components: {
+    breadcrumb: Breadcrumb,
+    unbagDetail: unbagDetail,
+    CameraScanner,
+  },
+  data() {
+    return {
+      title: "Unbagging",
+      item_code: "",
+      item_number: "",
+      form: {},
+      inputLabelPlaceholder: "Masukan code BAG",
+      radio_option: "connote",
+    };
+  },
+  methods: {
+    updateValue() {
+      this.form.item_number = this.item_code;
+      if (this.item_code !== null) {
+        this.item_number = this.item_code;
+        if (this.inputLabelPlaceholder.includes("CONNOTE")) {
+          this.item_number = this.item_code + "00";
         }
-    },
-    methods: {
-      updateValue(){
-        this.form.item_number = this.item_code
-        if(this.item_code !== null){
-          this.item_number = this.item_code
-          if (this.inputLabelPlaceholder.includes("CONNOTE")) {
-            this.item_number = this.item_code + "00"
-          }
 
-          if (this.radio_option === "connote") {
-            this.inputLabelPlaceholder = "Masukan code CONNOTE (ORION)"
-          }
-          else if (this.radio_option === "koli") {
-            this.inputLabelPlaceholder = "Masukan code KOLI"
-          }
+        if (this.radio_option === "connote") {
+          this.inputLabelPlaceholder = "Masukan code CONNOTE (ORION)";
+        } else if (this.radio_option === "koli") {
+          this.inputLabelPlaceholder = "Masukan code KOLI";
         }
-        this.$nextTick(() => {
-          this.handleClearForm()
-        });
-      },
+      }
+      this.$nextTick(() => {
+        this.handleClearForm();
+      });
+    },
 
-      handleClearForm(){
-        this.form = {}
-        this.item_code=''
-      },
-      
-    }
-}
+    handleClearForm() {
+      this.form = {};
+      this.item_code = "";
+    },
+
+    onCameraScannerGetData(data) {
+      if (
+        // eslint-disable-next-line operator-linebreak
+        data &&
+        // eslint-disable-next-line operator-linebreak
+        data.event === "result" &&
+        data.namespace === "formInputUnbagging"
+      ) {
+        this.item_code = data.data.text;
+        this.updateValue();
+      }
+    },
+  },
+};
 </script>
 <style lang="scss">
-    .bagging{
-        min-height: 50vh;
-        margin-top: 2em;
-    }
+.bagging {
+  min-height: 50vh;
+  margin-top: 2em;
+}
 
-    .in-get-bag{
-      font-size: 16px;
-    }
-    .logo{
-
-    }
-    .mt-2{
-      margin-top: 20px;
-    }
-
+.in-get-bag {
+  font-size: 16px;
+}
+.logo {
+}
+.mt-2 {
+  margin-top: 20px;
+}
 </style>
