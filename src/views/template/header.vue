7326fdc863e27c407a44365a1421af237605f541
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-04-17 16:23:37 +0700

    feat: add permission sidebar

--- src/views/template/header.vue
+++ src/views/template/header.vue
@@ -182,11 +182,13 @@ export default {
                                     obj["value"] = String(item.node_id)
                                     obj["node_code"] = String(item.node_code)
                                     obj["is_currently_used"] = item.is_currently_used
+                                    obj["permissions"] = item.permissions
 
                                     this.datanode.push(obj)
                     })
                     
                     let current_node = this.datanode.find(node => node.is_currently_used).value
+                    let permissions = this.datanode.find(node => node.is_currently_used).permissions
 
                     let n = this.$ls.get('node_id')
                     if(n == null) {
@@ -194,6 +196,7 @@ export default {
                         this.selectedNode = String(current_node)
                         this.$store.dispatch(`SET_USER_N`, this.datanode[0])
                     } else {
+                        this.$ls.set('permissions',permissions)
                         this.$store.dispatch(`SET_USER_N`, n)
                         this.selectedNode = String(n.value)
                     }
