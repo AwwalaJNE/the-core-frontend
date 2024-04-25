6c12b6e314e9d31b96b3a40b71d025a9434c29e9
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-04-25 07:59:23 +0700

    fix: issue create sm

--- src/views/transport/manifest/dialogCreateEditManifest.vue
+++ src/views/transport/manifest/dialogCreateEditManifest.vue
@@ -366,7 +366,7 @@ export default {
         .catch((error) => console.log("error", error));
     },
     formData(form) {
-      let node_id = this.listenActiveUser.nodes[0].node_id;
+      let node_id = form["node_id_origin"] ? form["node_id_origin"]["node_id"] : this.listenActiveUser.nodes[0].node_id;
       form["node_id_origin"] = node_id;
       form["node_id_destination"] = form["node_id_destination"]["node_id"];
 
