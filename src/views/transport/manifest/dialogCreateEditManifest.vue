02a5f6c72825d4fa9d48a422b5bd9ba80c8937c1
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-05-02 12:15:48 +0700

    adjust destination in surat muatan detail

--- src/views/transport/manifest/dialogCreateEditManifest.vue
+++ src/views/transport/manifest/dialogCreateEditManifest.vue
@@ -258,8 +258,7 @@ export default {
             data["bag_number"] = data.item_number;
             data["type"] = data.item_type;
             data["bag_weight"] = data.total_weight;
-            data["destination_name"] = data["bag"]["destination"]
-              ? data["bag"]["destination"]["node_tariff_code"] : "";
+            data["destination_name"] = data["bag"] ? data["bag"]["destination"]["node_tariff_code"] : "";
             arr.push(data);
           }
         });
