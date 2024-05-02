7ab5ce4cba933a982862b5b10e89340eac61a94e
AlifahJNE <alifah.rahmatika@jne.co.id>
2024-05-02 11:54:07 +0700

    fix sm item destination

--- src/views/transport/manifest/dialogCreateEditManifest.vue
+++ src/views/transport/manifest/dialogCreateEditManifest.vue
@@ -258,8 +258,8 @@ export default {
             data["bag_number"] = data.item_number;
             data["type"] = data.item_type;
             data["bag_weight"] = data.total_weight;
-            data["destination_name"] = data["destination"]["node_tariff_code"]
-              ?? "";
+            data["destination_name"] = data["bag"]["destination"]
+              ? data["bag"]["destination"]["node_tariff_code"] : "";
             arr.push(data);
           }
         });
@@ -660,8 +660,8 @@ export default {
             let arr = res.data.data;
             arr.map((item) => {
               item["type"] = "Bag";
-              item["destination_name"] = item["destination"]
-                ? item["destination"]["node_tariff_code"]
+              item["destination_name"] = item["bag"]["destination"]
+                ? item["bag"]["destination"]["node_tariff_code"]
                 : "";
             });
             this.dataTable = this.dataTable.concat(arr);
