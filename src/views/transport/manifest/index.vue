18ebc24205a49e37d53d93a06a1cbc21d2f503e6
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-05-14 15:32:18 +0700

    feat: add filter search by column and filter date

--- src/views/transport/manifest/index.vue
+++ src/views/transport/manifest/index.vue
@@ -21,12 +21,26 @@
     <section class="nodes">
       <div class="box view">
         <div class="nav-box">
-          <vs-row justify="">
-            <vs-col xs="3" sm="3" lg="3" style="margin-bottom: 10px;">
-              <daterange-filter @searchDate="searchDate" size="small" />
+          <vs-row justify="space-between">
+            <vs-col xs="12" sm="12" lg="6" class="mb-15">
+              <vs-row>
+                <vs-col w="4">
+                  <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateFilterDateBy" :valueData="dateParams" :selectedValue="filterDateBy" />
+                </vs-col>
+                <vs-col w="8">
+                  <daterange-filter @searchDate="searchDate" size="small" />
+                </vs-col>
+              </vs-row>
             </vs-col>
-            <vs-col offset="6" xs="6" sm="3" lg="3" class="mb-15" align="right">
-              <search-input ref="searchInput" @searchValue="searchValue" :placeholder="`Search SM`" />
+            <vs-col xs="12" sm="12" lg="6" class="mb-15">
+              <vs-row justify="end">
+                <vs-col xs="6" sm="8" lg="4">
+                  <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateSearchBy" :valueData="searchParams" :selectedValue="searchBy" />
+                </vs-col>
+                <vs-col xs="6" sm="4" lg="4">
+                  <search-input ref="searchInput" @searchValue="searchValue" :placeholder="searchPlaceholder" />
+                </vs-col>
+              </vs-row>
             </vs-col>
           </vs-row>
         </div>
@@ -36,6 +50,8 @@
               :ref="'suratMuatantable'"
               :dateFilter="tempDate"
               :query="tempSearch"
+              :searchBy="searchBy"
+              :filterDateBy="filterDateBy"
             />
           </transition>
         </template>
@@ -57,6 +73,7 @@ import master from "@/mixins/master";
 import Breadcrumb from "@/components/breadcrumb/index";
 import SearchInput from "@/components/search/searchInput";
 import dateRange from "@/components/daterange/index";
+import SelectSearchBy from "@/components/search/selectSearchBy";
 
 import SuratMuatantable from "@/views/transport/manifest/manifest";
 import SuratMuatanDialog from "@/views/transport/manifest/dialogCreateEditManifest";
@@ -68,6 +85,7 @@ export default {
     breadcrumb: Breadcrumb,
     "search-input": SearchInput,
     "daterange-filter": dateRange,
+    "select-search-by" : SelectSearchBy,
     suratMuatantable: SuratMuatantable,
     SuratMuatanDialog: SuratMuatanDialog,
   },
@@ -86,6 +104,53 @@ export default {
       ],
       node_request: "",
       status_pickup: "",
+      searchBy:"manifest number",
+      filterDateBy:"create",
+      searchPlaceholder: "Search Manifest Number",
+      searchParams: [
+        {
+          label: 'Manifest Number',
+          value: 'manifest number'
+        },
+        {
+          label: 'Type SM',
+          value: 'manifestMethod'
+        },
+        {
+          label: 'Jenis Kiriman',
+          value: 'vehicleType'
+        },
+        {
+          label: 'Origin',
+          value: 'origin'
+        },
+        {
+          label: 'Destination',
+          value: 'destination'
+        },
+        {
+          label: 'Weight',
+          value: 'weight'
+        },
+        {
+          label: 'Status',
+          value: 'status'
+        }
+      ],
+      dateParams: [
+        {
+          label: 'Created Date',
+          value: 'create'
+        },
+        {
+          label: 'ETD',
+          value: 'etd'
+        },
+        {
+          label: 'ETA',
+          value: 'eta'
+        }
+      ]
     };
   },
   methods: {
@@ -136,6 +201,14 @@ export default {
           this.loading = false;
           this.openNotification("danger", "Failed to populate node list", err);
         });
+    },
+    updateSearchBy(key, val) {
+      val = val.replaceAll(" ", "_");
+      this.searchBy = val;
+      this.searchPlaceholder = key;
+    },
+    updateFilterDateBy(key,val) {
+      this.filterDateBy = val;
     },
   },
   mounted() {
