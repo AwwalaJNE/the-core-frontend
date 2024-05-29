80c489b200e45a4711f92249e8589adf2075ee1a
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-05-29 16:17:33 +0700

    feat: add filter by column and filter date in vehicle and vehicle mode

--- src/views/settings/vehicles/vehicleList/index.vue
+++ src/views/settings/vehicles/vehicleList/index.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
+        <vs-row justify="space-between">
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row>
+                    <vs-col w="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateFilterDateBy"
+                            :valueData="dateParams" :selectedValue="filterDateBy" />
+                    </vs-col>
+                    <vs-col w="8">
+                        <date-time  :valueData="dateRange" typeInput="daterange" @updateValue="updateValue" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row justify="end">
+                    <vs-col xs="6" sm="8" lg="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateSearchBy"
+                            :valueData="searchParams" :selectedValue="searchBy" />
+                    </vs-col>
+                    <vs-col xs="6" sm="4" lg="4">
+                        <search-input ref="searchInput" @searchValue="searchValue" :placeholder="searchPlaceholder" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+        </vs-row>
         <table-master 
         :dataTable="dataTable" 
         :dataColumn="datacolumn" 
@@ -29,6 +53,10 @@ import axios from "axios";
 import master from "@/mixins/master"
 import TableMaster from "@/components/table/tableMaster.vue"
 import dialogCreateEditVehicle from "@/views/settings/vehicles/vehicleList/dialogCreateEditVehicle"
+import SelectSearchBy from "@/components/search/selectSearchBy"
+import SearchInput from "@/components/search/searchInput"
+import DateTime from "@/components/input/dateTime"
+
 export default {
     name:"vehicle-list",
     mixins: [master],
@@ -37,7 +65,10 @@ export default {
     },
     components: {
         "table-master" : TableMaster,
-        "dialog-create-edit-Vehicle": dialogCreateEditVehicle
+        "dialog-create-edit-Vehicle": dialogCreateEditVehicle,
+        "select-search-by": SelectSearchBy,
+        "search-input": SearchInput,
+        "date-time": DateTime,
     },
     data() {
         return {
@@ -82,7 +113,43 @@ export default {
                 limit:20,
                 page_size: 1,
                 page: 1
-            }
+            },
+            dateRange: [],
+            searchBy: "vehicle name",
+            filterDateBy: "create",
+            searchPlaceholder: "Search Vehicle Name",
+            searchParams: [
+            {
+                    label: "Vehicle ID",
+                    value: "vehicle_id"
+                },
+                {
+                    label: "Vehicle Name",
+                    value: "vehicle name"
+                },
+                {
+                    label: "Vehicle police no",
+                    value: "vehicle_police_no"
+                },
+                {
+                    label: "Vehicle owned by",
+                    value: "vehicle_owned_by"
+                },
+                {
+                    label: "Max Weight",
+                    value: "vehicle_max_weight"
+                },
+                {
+                    label: "Max Volume",
+                    value: "vehicle_max_volume"
+                },
+            ],
+            dateParams: [
+              {
+                label: 'Created Date',
+                value: 'create'
+              }
+            ]
         }
     },
     watch: {
@@ -96,15 +163,21 @@ export default {
         }
     },
     methods: {
-        async getTableData(limit,page,q) {
+        async getTableData(limit,page,q,from,to) {
             this.loading = true
             let query = "";
+            let startDate = "";
+            let endDate = "";
             if(q !== undefined) {
                 query = q
             }
+            if(from !== undefined && to !== undefined) {
+              startDate = from
+              endDate = to
+            }
             await axios
                 .get(this.URL.vehicle + 
-                `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}`, 
+                `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}&start_date=${startDate}&end_date=${endDate}&search_by=${this.searchBy}&filter_date_by=${this.filterDateBy}`, 
                 this.Helper.header())
                 .then(res => {
                     
@@ -176,12 +249,34 @@ export default {
             this.refresh()
         },
         refresh(){
-            console.log('masukkk');
-            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch)
+            let from = ''
+            let to = ''
+
+            if(this.dateRange != null && this.dateRange.length > 0) {
+                from = moment(this.dateRange[0]).format("YYYY-MM-DD")
+                to = moment(this.dateRange[1]).format("YYYY-MM-DD")
+            }
+            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch,from,to)
         },
         closeDialogVehicle() {
             this.dialogVehicle = false
-        }
+        },
+        updateValue(key, val) {
+            this.dateRange = val
+            this.refresh()
+        },
+        searchValue (val) {
+            this.tempSearch = val
+            this.refresh()
+        },
+        updateSearchBy(key, val) {
+            val = val.replaceAll(" ", "_");
+            this.searchBy = val;
+            this.searchPlaceholder = key;
+        },
+        updateFilterDateBy(key, val) {
+            this.filterDateBy = val;
+        },
     },
     mounted() {
         this.refresh()
--- src/views/settings/vehicles/vehicleMode/index.vue
+++ src/views/settings/vehicles/vehicleMode/index.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
+        <vs-row justify="space-between">
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row>
+                    <vs-col w="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateFilterDateBy"
+                            :valueData="dateParams" :selectedValue="filterDateBy" />
+                    </vs-col>
+                    <vs-col w="8">
+                        <date-time :valueData="dateRange" typeInput="daterange" @updateValue="updateValue" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row justify="end">
+                    <vs-col xs="6" sm="8" lg="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateSearchBy"
+                            :valueData="searchParams" :selectedValue="searchBy" />
+                    </vs-col>
+                    <vs-col xs="6" sm="4" lg="4">
+                        <search-input ref="searchInput" @searchValue="searchValue" :placeholder="searchPlaceholder" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+        </vs-row>
         <table-master 
         :dataTable="dataTable" 
         :dataColumn="datacolumn" 
@@ -29,6 +53,10 @@ import axios from "axios";
 import master from "@/mixins/master"
 import TableMaster from "@/components/table/tableMaster.vue"
 import dialogCreateEditVehicleMode from "@/views/settings/vehicles/vehicleMode/dialogCreateEditVehicleMode"
+import SelectSearchBy from "@/components/search/selectSearchBy"
+import SearchInput from "@/components/search/searchInput"
+import DateTime from "@/components/input/dateTime"
+
 export default {
     name:"vehicle-mode",
     mixins: [master],
@@ -37,7 +65,10 @@ export default {
     },
     components: {
         "table-master" : TableMaster,
-        "dialog-create-edit-VehicleMode": dialogCreateEditVehicleMode
+        "dialog-create-edit-VehicleMode": dialogCreateEditVehicleMode,
+        "select-search-by": SelectSearchBy,
+        "search-input": SearchInput,
+        "date-time": DateTime,
     },
     data() {
         return {
@@ -62,7 +93,27 @@ export default {
                 limit:20,
                 page_size: 1,
                 page: 1
-            }
+            },
+            dateRange: [],
+            searchBy: "vehicle mode id",
+            filterDateBy: "create",
+            searchPlaceholder: "Search Vehicle Mode Name",
+            searchParams: [
+                {
+                    label: "Vehicle Mode ID",
+                    value: "vehicle mode id",
+                },
+                {
+                    label: "Vehicle Mode Name",
+                    value: "vehicle_mode_name",
+                },
+            ],
+            dateParams: [
+              {
+                label: 'Created Date',
+                value: 'create'
+              }
+            ]
         }
     },
     watch: {
@@ -76,15 +127,21 @@ export default {
         }
     },
     methods: {
-        async getTableData(limit,page,q) {
+        async getTableData(limit,page,q,from,to) {
             this.loading = true
             let query = "";
+            let startDate = "";
+            let endDate = "";
             if(q !== undefined) {
                 query = q
             }
+            if(from !== undefined && to !== undefined) {
+              startDate = from
+              endDate = to
+            }
             await axios
                 .get(this.URL.vehicle_mode + 
-                `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}`, 
+                `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}&start_date=${startDate}&end_date=${endDate}&search_by=${this.searchBy}&filter_date_by=${this.filterDateBy}`, 
                 this.Helper.header())
                 .then(res => {
                     console.log(res)
@@ -150,12 +207,34 @@ export default {
             this.refresh()
         },
         refresh(){
-            console.log("refresh")
-            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch)
+            let from = ''
+            let to = ''
+
+            if(this.dateRange != null && this.dateRange.length > 0) {
+                from = moment(this.dateRange[0]).format("YYYY-MM-DD")
+                to = moment(this.dateRange[1]).format("YYYY-MM-DD")
+            }
+            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch,from,to)
         },
         closeDialogVehicleMode() {
             this.dialogVehicleMode = false
-        }
+        },
+        updateValue(key, val) {
+            this.dateRange = val
+            this.refresh()
+        },
+        searchValue (val) {
+            this.tempSearch = val
+            this.refresh()
+        },
+        updateSearchBy(key, val) {
+            val = val.replaceAll(" ", "_");
+            this.searchBy = val;
+            this.searchPlaceholder = key;
+        },
+        updateFilterDateBy(key, val) {
+            this.filterDateBy = val;
+        },
     },
     mounted() {
         this.refresh()
