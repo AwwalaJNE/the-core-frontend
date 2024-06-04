1ae4e1b77f655ce229558a5b8d914fb0d3fa821f
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-06-04 16:39:34 +0700

    fix user

--- src/views/settings/users/user/userList.vue
+++ src/views/settings/users/user/userList.vue
@@ -7,6 +7,31 @@
 
 <template>
     <div>
+        <vs-row justify="space-between">
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row>
+                    <vs-col w="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateFilterDateBy"
+                            :valueData="dateParams" :selectedValue="filterDateBy" />
+                    </vs-col>
+                    <vs-col w="8">
+                        <date-time :name="''" :rules="''" :valueData="dateRange"
+                            typeInput="daterange" @updateValue="updateValue" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+            <vs-col xs="12" sm="12" lg="6">
+                <vs-row justify="end">
+                    <vs-col xs="6" sm="8" lg="4">
+                        <select-search-by :isMultiple="false" :border="true" @updateSearchBy="updateSearchBy"
+                            :valueData="searchParams" :selectedValue="searchBy" />
+                    </vs-col>
+                    <vs-col xs="6" sm="4" lg="4">
+                        <search-input ref="searchInput" @searchValue="searchValue" :placeholder="searchPlaceholder" />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+        </vs-row>
         <table-master
         :dataTable="dataTable" 
         :dataColumn="datacolumn" 
@@ -41,6 +66,10 @@ import axios from "axios";
 import master from "@/mixins/master"
 import TableMaster from "@/components/table/tableMaster.vue"
 import DialogCreateEditUser from "@/views/settings/users/user/dialogCreateEditUser"
+import SelectSearchBy from "@/components/search/selectSearchBy"
+import SearchInput from "@/components/search/searchInput"
+import DateTime from "@/components/input/dateTime"
+import moment from "moment"
 export default {
     name:"list-user",
     mixins: [master],
@@ -49,7 +78,10 @@ export default {
     },
     components: {
         "table-master" : TableMaster,
-        "dialog-create-edit-user": DialogCreateEditUser
+        "dialog-create-edit-user": DialogCreateEditUser,
+        "select-search-by": SelectSearchBy,
+        "search-input": SearchInput,
+        "date-time": DateTime
     },
     watch: {
         query: function(val, old) {
@@ -99,20 +131,58 @@ export default {
                 limit:20,
                 page_size: 1,
                 page: 1
-            }
+            },
+            dateRange: [],
+            searchBy: "user_login",
+            filterDateBy: "create",
+            searchPlaceholder: "Search User Login",
+            searchParams: [
+                {
+                    label: "Name",
+                    value: "user_name",
+                },
+                {
+                    label: "Username",
+                    value: "user_login",
+                },
+                {
+                    label: "Email",
+                    value: "user_email",
+                },
+                {
+                    label: "Primary Roles",
+                    value: "user_role_name",
+                },
+                {
+                    label: "Node",
+                    value: "userNodes"
+                },
+            ],
+            dateParams: [
+              {
+                label: 'Created Date',
+                value: 'create'
+              }
+            ]
         }
     },
     methods: {
-        async getTableData(limit,page,q) {
+        async getTableData(limit,page,q,from,to) {
             this.loading = true
             let query = "";
+            let startDate = "";
+            let endDate = "";
             if(q !== undefined) {
                 query = q
+            }
+            if(from !== undefined && to !== undefined) {
+              startDate = from
+              endDate = to
             }
             await axios
                 .get(
                     this.URL.user + 
-                    `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}`, 
+                    `?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}&s=${query}&start_date=${startDate}&end_date=${endDate}&search_by=${this.searchBy}&filter_date_by=${this.filterDateBy}`, 
                     this.Helper.header())
                 .then(res => {
                     let arr = res.data.data
@@ -210,7 +280,14 @@ export default {
             this.refresh()
         },
         refresh(val){
-            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch)
+            let from = ''
+            let to = ''
+
+            if(this.dateRange != null && this.dateRange.length > 0) {
+                from = moment(this.dateRange[0]).format("YYYY-MM-DD")
+                to = moment(this.dateRange[1]).format("YYYY-MM-DD")
+            }
+            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch,from,to)
         },
         closeDialogUser(){
             this.dialogUser = false
@@ -222,7 +299,23 @@ export default {
         },
         finishGetUser(){
             this.loading = false
-        }
+        },
+        updateValue(key, val) {
+            this.dateRange = val
+            this.refresh()
+        },
+        searchValue (val) {
+            this.tempSearch = val
+            this.refresh()
+        },
+        updateSearchBy(key, val) {
+            val = val.replaceAll(" ", "_");
+            this.searchBy = val;
+            this.searchPlaceholder = key;
+        },
+        updateFilterDateBy(key, val) {
+            this.filterDateBy = val;
+        },
     },
     mounted() {
         this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch)
