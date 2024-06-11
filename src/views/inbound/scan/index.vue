49d35d0ce7db4b5b97df0b8e2b616fb29712525e
Sarah Fahrurrozi <132034019+sarahfahrurrozi@users.noreply.github.com>
2024-06-11 15:52:21 +0700

    hot fix label

--- src/views/inbound/scan/index.vue
+++ src/views/inbound/scan/index.vue
@@ -22,7 +22,7 @@
                         <div class="center">
                           <vs-input border type="text"
                                     v-model="item_no"
-                                    label-placeholder="Masukkan code BAG / Connote / SM"
+                                    label-placeholder="Masukkan code BAG / Koli / SM"
                                     autofocus
                                     icon-after
                                     ref="formInputInbound"
