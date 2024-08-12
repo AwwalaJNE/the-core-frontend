7925b570bb55d6ac9eaead4f475aa0de89e0550b
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-08-12 17:05:52 +0700

    add dialog cancel in configuration warning runsheet

--- src/views/settings/configurationWarningRunsheet/warningRunsheet/index.vue
+++ src/views/settings/configurationWarningRunsheet/warningRunsheet/index.vue
@@ -23,12 +23,23 @@
             :dataItem="dataItem"
             @refresh="refresh"
         />
+
+        <dialog-confirm
+            title="Remove Configuration Warning Runsheet"
+            message="Are you sure you want to remove this configuration warning runsheet?"
+            :active="activeDialogConfirmRemove"
+            :loading="loadingConfirmRemove"
+            :closeDialog="closeDialogConfirmRemove"
+            @confirm="confirmRemove"
+            @cancel="closeDialogConfirmRemove"
+        />
     </div>
 </template>
 <script>
 import axios from "axios";
 import master from "@/mixins/master"
 import TableMaster from "@/components/table/tableMaster.vue"
+import DialogConfirm from "@/components/dialog/dialogConfirm"
 import DialogCreateEditConfigurationWarningRunsheet from "@/views/settings/configurationWarningRunsheet/warningRunsheet/dialogCreateEditConfigurationWarningRunsheet"
 import SearchInput from "@/components/search/searchInput"
 import Inputan from "@/components/input/inputan"
@@ -47,6 +58,7 @@ export default {
         "search-input": SearchInput,
         "date-time": DateTime,
         "inputan": Inputan,
+        "dialog-confirm": DialogConfirm,
     },
     data() {
         return {
@@ -81,12 +93,15 @@ export default {
             loading: false,
             dataItem: {},
             searchValue: this.query ? this.query : "",
-            dialogConfigurationWarningRunsheet: false,
             pagination: {
                 limit: 20,
                 page_size: 1,
                 page: 1
             },
+            id: '',
+            dialogConfigurationWarningRunsheet: false,
+            activeDialogConfirmRemove: false,
+            loadingConfirmRemove:false,
         }
     },
     computed: {
@@ -164,19 +179,6 @@ export default {
                 });
             }
         },
-        async actionRemove(val){
-            await axios
-                .delete(
-                    this.URL.configuration_warning_runsheet + `/${val.setting_id}?n=${this.listenNodeId}`,
-                    this.Helper.header())
-                .then(res => {
-                    this.refresh()
-                    this.openNotification(null, 'Success', 'Delete Warning Configuration is success')
-                }).catch(err => {
-                    this.loading = false
-                    this.openNotification('danger', 'Delete Warning Configuration is failed', err.response.data.message)
-                })
-        },
         actionLimit(val){
             this.pagination.limit = val
             this.pagination.page = 1
@@ -200,7 +202,36 @@ export default {
             this.$store.dispatch("SET_CONFIGURATION_WARNING_RUNSHEET_SETTING_ID_visible", false)
             this.dialogConfigurationWarningRunsheet = false
             this.refresh()
-        },   
+        },
+        actionRemove(val){
+            this.id = val.setting_id;
+            this.activeDialogConfirmRemove = true
+        },
+        confirmRemove() {
+            this.loadingConfirmRemove=true
+            this.removeData()
+        },
+        async removeData(){
+            await axios
+                .delete(
+                    this.URL.configuration_warning_runsheet + `/${this.id}?n=${this.listenNodeId}`,
+                    this.Helper.header())
+                .then(res => {
+                    this.closeDialogConfirmRemove()
+                    this.loadingConfirmRemove = false
+                    this.refresh()
+                    this.openNotification(null, 'Success', 'Delete Warning Configuration is success')
+                }).catch(err => {
+                    this.loadingConfirmRemove = false
+                    this.closeDialogConfirmRemove()
+                    this.loading = false
+                    this.openNotification('danger', 'Delete Warning Configuration is failed', err.response.data.message)
+                })
+        },
+        closeDialogConfirmRemove(){
+            this.activeDialogConfirmRemove = false
+            this.loadingConfirmRemove=false
+        },
     },
     mounted() {
         this.refresh()
