0f437e999ec44bde7558839911e470a77d411a26
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-08-14 15:41:00 +0700

    change position of list item

--- src/views/irreguralities/entryStatus/dialogEntryStatus.vue
+++ src/views/irreguralities/entryStatus/dialogEntryStatus.vue
@@ -12,6 +12,24 @@
 
         <template v-slot:content>
             <vs-row justify="space-between">
+                <vs-row>
+                    <vs-col xs="12" sm="12" lg="6">
+                        <input-text-area 
+                            id="valid_item"
+                            label="Valid Bag / Connote"
+                            v-model="validItemNumber"
+                            :disabled="true"
+                        />
+                    </vs-col>
+                    <vs-col xs="12" sm="12" lg="6">
+                        <input-text-area 
+                            id="invalid_item"
+                            label="Invalid Bag / Connote"
+                            v-model="invalidItemNumber"
+                            :disabled="true"
+                        />
+                    </vs-col>
+                </vs-row>
                 <vs-col xs="12" sm="12" lg="12">
                     <template v-if="loadingStatus == false && status_code_arr.length > 0">
                         <selector 
@@ -121,24 +139,6 @@
                         @updateValue="updateValue" 
                     />
                 </vs-col>
-                <vs-row>
-                    <vs-col xs="12" sm="12" lg="6">
-                        <input-text-area 
-                            id="valid_item"
-                            label="Valid Bag / Connote"
-                            v-model="validItemNumber"
-                            :disabled="true"
-                        />
-                    </vs-col>
-                    <vs-col xs="12" sm="12" lg="6">
-                        <input-text-area 
-                            id="invalid_item"
-                            label="Invalid Bag / Connote"
-                            v-model="invalidItemNumber"
-                            :disabled="true"
-                        />
-                    </vs-col>
-                </vs-row>
             </vs-row>
         </template>
 
