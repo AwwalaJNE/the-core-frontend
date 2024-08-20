2c85c4a4d72e8203706f37a4564385c9abc14873
AlifahJNE <alifah.rahmatika@jne.co.id>
2024-08-20 15:58:38 +0700

    feat: add unit test for inbound scan

---
+++ tests/jest.setup.js
@@ -0,0 +1,42 @@
+import VueRouter from 'vue-router';
+import Vuesax from 'vuesax';
+import Vuex from 'vuex';
+import { createLocalVue } from '@vue/test-utils';
+
+// Mock global components and libraries
+jest.mock('axios');
+
+jest.mock('@/components/scanner/camera.vue', () => ({
+    name: 'CameraScanner',
+    template: '<div></div>',
+    methods: {
+        open: jest.fn(),
+        close: jest.fn()
+    }
+}));
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+localVue.use(Vuesax);
+localVue.use(Vuex);
+
+// Set up Vuex store
+const state = {
+  user: {
+    node_id: {
+      value: '2301',
+      node_code: 'KP-CGK-001'
+    }
+  }
+};
+
+const getters = {
+  getUser: (state) => state.user
+};
+
+const store = new Vuex.Store({
+  state,
+  getters
+});
+
+export { localVue, store };
