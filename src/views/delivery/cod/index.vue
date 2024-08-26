4f33972ca9bf6762a28f83147e3c749e6b99ed3c
AlifahJNE <alifah.rahmatika@jne.co.id>
2024-08-26 15:29:05 +0700

    feat: update outstanding cod datatable

--- src/views/delivery/cod/index.vue
+++ src/views/delivery/cod/index.vue
@@ -13,12 +13,15 @@
       <div class="box view">
         <div class="nav-box">
           <vs-row justify="end">
-            <vs-col xs="6" sm="8" lg="4">
+            <vs-col xs="12" sm="12" lg="7" style="margin-bottom: 0.5rem;">
+              <daterange-filter @searchDate="searchDate" size="small" />
+            </vs-col>
+            <vs-col xs="6" sm="8" lg="3">
               <select-search-by
                 :valueData="filterValues"
                 :isMultiple="false"
                 :border="true"
-                :selectedValue="searchByVal"
+                :selectedValue="searchBy"
                 @updateSearchBy="updateSearchBy"
               />
             </vs-col>
@@ -26,7 +29,7 @@
               <search-input
                 ref="searchInput"
                 @searchValue="searchValue"
-                class="search-input"
+                :placeholder="searchPlaceholder"
               />
             </vs-col>
           </vs-row>
@@ -35,40 +38,26 @@
           <transition name="slide-fade">
             <DeliveryCodTable
               :ref="'DeliveryCodTable'"
-              :node="node_request"
               :dateFilter="tempDate"
               :query="tempSearch"
-              :searchBy="searchByVal"
+              :searchBy="searchBy"
               @openDialog="openDialog"
             />
           </transition>
         </template>
-        <dialog-cod-collect
-          :active="dialogCodCollect"
-          :closeDialog="closeDialogCodCollect"
-          :expectedAmount="expectedAmount"
-          :runsheetNumber="runsheetNumber"
-          :courierId="courierId"
-          title="Collect COD"
-          @confirm="confirm"
-          @cancel="closeDialogCodCollect"
-          @refresh="refresh"
-        />
       </div>
     </section>
   </div>
 </template>
 <script>
-import axios from "axios";
 import master from "@/mixins/master";
 import NavItem from "@/components/navbar/navTab";
 import Breadcrumb from "@/components/breadcrumb/index";
 import SearchInput from "@/components/search/searchInput";
-import dateRange from "@/components/daterange/index";
 import SelectSearchBy from "@/components/search/selectSearchBy";
+import DateRange from "@/components/daterange/index";
 
 import DeliveryCodTable from "@/views/delivery/cod/codTable";
-import DialogCodCollect from "@/views/delivery/cod/dialogCodCollect";
 
 export default {
   name: "Inbound-List",
@@ -77,51 +66,39 @@ export default {
     "nav-item": NavItem,
     breadcrumb: Breadcrumb,
     "search-input": SearchInput,
-    "daterange-filter": dateRange,
     DeliveryCodTable: DeliveryCodTable,
     "select-search-by": SelectSearchBy,
-    "dialog-cod-collect": DialogCodCollect,
+    "daterange-filter": DateRange,
   },
   data() {
     return {
-      title: "SCO",
+      title: "Deposit COD",
       tempSearch: "",
       tempDate: [],
-      DataNode: [],
-      node_request: "",
-      node_origin: "",
-      node_destination: "",
-      searchByVal: "courier_employee_name",
-      searchPlaceholder: "Search Courier Name",
-      filterDateBy:"updated",
+      searchBy: "hrs_number",
+      searchPlaceholder: "Search HRS Number",
       filterValues: [
         {
-          label: "Nama Kurir",
-          value: "courier_employee_name",
+          label: "HRS Number",
+          value: "hrs_number",
         },
         {
-          label: "Total Connotes",
-          value: "count_connote",
+          label: "Courier Name",
+          value: "courier_name",
+        },
+        {
+          label: "Courier Code",
+          value: "courier_code",
         },
         {
           label: "Runsheet",
           value: "delivery_runsheet_number",
         },
         {
-          label: "HRS",
-          value: "hrs",
-        },
-        {
-          label: "Total COD (Rp)",
-          value: "count_cod",
-          type_amount: true,
-                    textAlign: "right"
+          label: "DRI Number",
+          value: "dri_number",
         },
       ],
-      dialogCodCollect: false,
-      expectedAmount: 0,
-      runsheetNumber: "",
-      courierId: ""
     };
   },
   methods: {
@@ -131,74 +108,16 @@ export default {
     searchValue(val) {
       this.tempSearch = val;
     },
+    searchDate(val) {
+      this.tempDate = val;
+    },
     clearSearch() {
       this.$refs.searchInput.clear();
     },
-
-    openDialog(amount, runsheet, courier) {
-      this.dialogCodCollect = true
-      this.expectedAmount = amount
-      this.runsheetNumber = runsheet
-      this.courierId = courier
+    updateSearchBy(key, val) {
+      this.searchBy = val;
+      this.searchPlaceholder = key;
     },
-
-    closeDialogCodCollect() {
-      this.dialogCodCollect = false
-    },
-
-    async getDataNodeType() {
-      this.loading = true;
-      await axios
-        .get(
-          this.URL.node_type +
-            `?n=${this.listenNodeId}&sort_order=desc&&limit=1000&page=1&s=`,
-          this.Helper.header()
-        )
-        .then((res) => {
-
-          if (res.data.data.length > 0) {
-            res.data.data.map((item) => {
-              let obj = {};
-              obj["label"] = item.node_type_name;
-              obj["value"] = item.node_type_id;
-
-              this.DataNode.push(obj);
-            });
-          }
-
-          this.loading = false;
-        })
-        .catch((err) => {
-          this.loading = false;
-          this.openNotification("danger", err.response ? err.response.data.code : '', "Failed to populate node list", err);
-        });
-    },
-    updateNode(val) {},
-
-    updateSearchBy(_, val) {
-      if (val && this.filterValues.map((v) => v.value).includes(val)) {
-        this.searchByVal = val;
-      }
-    },
-  },
-  mounted() {
-    this.getDataNodeType();
   },
 };
 </script>
-
-<style lang="scss">
-.mb-15 {
-  margin-bottom: 1.5em;
-}
-
-.custom-title {
-  padding: 0.6em;
-  text-align: right;
-  font-weight: 600;
-}
-
-.pb-10 {
-  padding-bottom: 10px;
-}
-</style>
