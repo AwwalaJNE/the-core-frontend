706f96930e8c9ca39ee3496910d79d141c05a0e9
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-09-03 10:09:04 +0700

    adjust default value for switch

--- src/views/transport/manifest/dialogCreateEditManifest.vue
+++ src/views/transport/manifest/dialogCreateEditManifest.vue
@@ -267,6 +267,9 @@ export default {
         this.getDataVehicle();
       }
 
+      this.via_sj = !!this.listenDataItem["departed_time"];
+      this.$store.dispatch("SET_SURAT_MUATAN_VIA_SJ", this.via_sj);
+
       if (this.listenDataItem.hasOwnProperty("detail")) {
         let arr = [];
         this.listenDataItem["detail"].map((data) => {
@@ -853,6 +856,8 @@ export default {
 
       this.$store.dispatch("SET_SURAT_MUATAN_VEHICLE_ID", "");
       this.$store.dispatch("SET_SURAT_MUATAN_VEHICLE_ID_ArrData", []);
+
+      this.$store.dispatch("SET_SURAT_MUATAN_VIA_SJ", true);
     },
     handleEta(dateTime, amount) {
       if (dateTime && amount) {
