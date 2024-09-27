c83745578c2d2e0c92b26b7b2c5e20cbd0deabec
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-09-27 07:35:24 +0700

    remove unused item

--- src/views/delivery/runsheetNew/index.vue
+++ src/views/delivery/runsheetNew/index.vue
@@ -106,7 +106,7 @@ export default {
     },
     data() {
         return {
-            title: "Assign Halo",
+            title: "Assign",
             tempSearch: "",
             tempDate: moment().format("YYYY-MM-DD"),
             DataNode: [],
