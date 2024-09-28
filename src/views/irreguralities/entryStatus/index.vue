3482926bda1da3ebbeb6c947a464b8eb61d2f70e
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-09-28 18:55:17 +0700

    add column and search orion sequence in irregularity

--- src/views/irreguralities/entryStatus/index.vue
+++ src/views/irreguralities/entryStatus/index.vue
@@ -194,6 +194,12 @@ export default {
                     label: "Date",
                     key: "created_at",
                     width: "md"
+                    width: "sm"
+                },
+                {
+                    label: "Orion Number",
+                    key: "irg_sequence",
+                    width: "xs"
                 },
                 {
                     label: "Bag Number",
@@ -245,6 +251,10 @@ export default {
                 {
                     label: 'Koli number',
                     value: 'koli_number'
+                },
+                {
+                    label: "Orion Number",
+                    value: "irg_sequence",
                 },
                 {
                     label: 'Bag number',
