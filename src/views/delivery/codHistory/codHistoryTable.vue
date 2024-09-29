07592eeb9310f9d956a45a1b32fbffa55ba10a57
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-09-29 19:21:27 +0700

    remove unused item

--- src/views/delivery/codHistory/codHistoryTable.vue
+++ src/views/delivery/codHistory/codHistoryTable.vue
@@ -143,7 +143,6 @@ export default {
           this.Helper.header()
         )
         .then((res) => {
-          let arr = res.data.data
           let is_cdm = this.listenNodeIsCDM ? ["TRB", "CDM"] : ["TRB"];
           let arr = res.data.data.filter(item => is_cdm.includes(item.deposit_method));
 
