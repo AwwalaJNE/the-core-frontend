a5abb931f2e4aaca1369b7e10b3ec0b0258ed195
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-10-01 19:04:01 +0700

    add master page runsheet

---
+++ src/views/helpdesk/runsheet/runsheetTable.vue
@@ -0,0 +1,193 @@
+<template>
+    <div>
+        <table-master 
+            :dataTable="dataTable" 
+            :dataColumn="datacolumn" 
+            :tableLoading="loading"
+            :pageSize="pagination.page_size"
+            :page="pagination.page"
+            :limit="pagination.limit"
+            :hasAction="false"
+            :hasLinked="['delivery_runsheet_number']"
+            :hasPagination="true"
+            :expandable="true"
+            @actionLimit="actionLimit"
+            @actionPagination="actionPagination"
+            @handleEdit="showData"
+        />
+
+    </div>
+</template>
+<script>
+import axios from "axios";
+import master from "@/mixins/master";
+
+import TableMaster from "@/components/table/tableMaster";
+
+export default {
+    name:"delivery-runsheet",
+    mixins: [master],
+    props: {
+        dateFilter: String,
+        filterDateBy: String,
+        query: String,
+        searchBy: String,
+    },
+    components: {
+        "table-master" : TableMaster
+    },
+    data() {
+        return {
+            dataTable: [],
+            datacolumn: [
+                {
+                    label: "Employee NIK",
+                    key: "employee_nik",
+                    width: "xs"
+                },
+                {
+                    label: "Employee Code",
+                    key: "employee_code",
+                    width: "xs"
+                },
+                {
+                    label: "Employee Name",
+                    key: "employee_name",
+                    width: "xs"
+                },
+                {
+                    label: "Delivery Runsheet Number",
+                    key: "delivery_runsheet_number",
+                    width: "sm"
+                },
+                {
+                    label: "DRI Number",
+                    key: "dri",
+                    width: "sm"
+                },
+                {
+                    label: "HRS Number",
+                    key: "total_runsheet",
+                    width: "sm"
+                },
+                {
+                    label: "Total Koli",
+                    key: "total_koli",
+                    width: "xs"
+                },
+                {
+                    label: "Open",
+                    key: "total_open",
+                    width: "xs"
+                },
+                {
+                    label: "Delivered",
+                    key: "total_delivered",
+                    width: "xs"
+                },
+                {
+                    label: "Undelivered",
+                    key: "total_undelivered",
+                    width: "xs"
+                },
+                {
+                  label: "Undelivered Receiving",
+                  key: "total_undelivery_received",
+                  width: "xs"
+                },
+                {
+                  label: "Created At",
+                  key: "created_at",
+                  width: "xs"
+                }
+            ],
+            loading: false,
+            dataItem: {},
+            tempSearch: "",
+            tempDate: [],
+            date: "",
+            pagination: {
+                limit: 20,
+                page_size: 1,
+                page: 1
+            }
+        }
+    },
+    watch: {
+        query: function(val, old) {
+            if(val !== undefined) {
+                this.tempSearch = val
+                if(this.tempSearch !== old) {
+                    this.getTableData(this.pagination.limit, 1, val, this.startDate, this.endDate)
+                }
+            }
+        },
+        dateFilter: function(val, old) {
+            if(val !== undefined) {
+                this.tempDate = val
+                if(this.tempDate !== old ) {
+                    this.startDate = this.tempDate !== null ? this.tempDate[0] : ''
+                    this.endDate = this.tempDate !== null ? this.tempDate[1] : ''
+                }
+                this.getTableData(this.pagination.limit, 1, this.tempSearch, this.startDate, this.endDate, this.node_filter)
+            }
+        }
+    },
+    methods: {
+        async getTableData(limit,page,q, from, to) {
+            this.loading = true
+            let query = "";
+            let startDate = "";
+            let endDate = "";
+            if(q !== undefined) {
+                query = q
+            }
+            if(from !== undefined && to !== undefined) {
+                startDate = from
+                endDate = to
+            }
+            await axios
+                .get(this.URL.revamp_delivery +
+                `?n=${this.listenNodeId}&s=${query}&date_filter=${this.dateFilter}&search_by=${this.searchBy}&page=${page}&limit=${limit}`,
+                this.Helper.header())
+                .then(res => {
+                    let arr = res.data.data
+                    this.dataTable = arr
+                    this.pagination.page = res.data.meta.current_page
+                    this.pagination.limit = parseInt(res.data.meta.per_page)
+                    this.pagination.page_size = res.data.meta.last_page
+                    this.loading = false
+                }).catch(err => {
+                    this.loading = false
+                    this.openNotification('danger', err?.response?.data?.code ?? '', err?.response?.data?.message ?? 'Failed to populate Delivery Runsheet list', err?.response?.data?.message ?? 'something went wrong');
+                })
+        },
+        actionLimit(val){
+            this.pagination.limit = val
+            this.pagination.page = 1
+            this.refresh()
+        },
+        actionPagination(val) {
+            this.pagination.page = val
+            this.refresh()
+        },
+        refresh(){
+            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch, this.startDate, this.endDate)
+        },
+        showData(row) {
+            console.log("PP", row)
+            this.$router.push({ 
+                name: 'delivery-runsheet-edit', 
+                params: { 
+                    employee_id: this.selectedCourier,
+                    delivery_runsheet_number: this.delivery_runsheet_number,
+                    date_filter: this.tempDate
+                } 
+            });
+        },
+    },
+    mounted() {
+        this.refresh()
+    }
+}
+</script>
