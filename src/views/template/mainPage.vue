209ed9e2e4227183d19f9bfa92937a6c816e2b6b
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-10-14 10:48:34 +0700

    add basic chart for new insight in main page

--- src/views/template/mainPage.vue
+++ src/views/template/mainPage.vue
@@ -34,16 +34,69 @@
                     </vs-col>
                 </vs-row>
             </div>
+
+            <vs-row>
+                <vs-col xs="12" sm="6" lg="3">
+                    <div class="box">
+                        <h4>Insight Inventory</h4>
+                        <bar-chart 
+                            ref="insightInventoryChart" 
+                            :chart-data="dataInsightInventory" 
+                            :options="optionsInsightInventory" 
+                        />
+                    </div>
+                </vs-col>
+
+                <vs-col xs="12" sm="6" lg="3">
+                    <div class="box">
+                        <h4>Insight Runsheet</h4>
+                        <doughnut-chart 
+                            ref="insightRunsheetChart" 
+                            :chart-data="dataInsightRunsheet" 
+                            :options="optionsInsightRunsheet" 
+                        />
+                    </div>
+                </vs-col>
+
+                <vs-col xs="12" sm="6" lg="3">
+                    <div class="box">
+                        <h4>Insight Receive</h4>
+                        <bar-chart 
+                            ref="insightReceiveChart" 
+                            :chart-data="dataInsightReceive" 
+                            :options="optionsInsightReceive" 
+                        />
+                    </div>
+                </vs-col>
+
+                <vs-col xs="12" sm="6" lg="3">
+                    <div class="box">
+                        <h4>Insight Depart</h4>
+                        <bar-chart 
+                            ref="insightDepartChart" 
+                            :chart-data="dataInsightDepart" 
+                            :options="optionsInsightDepart" 
+                        />
+                    </div>
+                </vs-col>
+            </vs-row>
         </vs-col>
     </vs-row>
 </template>
 <script>
 
+import axios from "axios";
 import master from "@/mixins/master";
+
+import { Bar, Doughnut } from 'vue-chartjs'
 
 export default {
     name: "main-page",
     mixins: [master],
+    components: {
+        "bar-chart": Bar,
+        "doughnut-chart": Doughnut
+    },
     computed: {
         listenUser() {
             return this.listenActiveUser;
@@ -54,6 +107,18 @@ export default {
             if (val) {
                 getInfo(val);
             }
+        },
+        dataInsightInventory(newData) {
+            this.$refs.insightInventoryChart.renderChart(newData, this.optionsInsightInventory);
+        },
+        dataInsightRunsheet(newData) {
+            this.$refs.insightRunsheetChart.renderChart(newData, this.optionsInsightRunsheet);
+        },
+        dataInsightReceive(newData) {
+            this.$refs.insightReceiveChart.renderChart(newData, this.optionsInsightReceive);
+        },
+        dataInsightDepart(newData) {
+            this.$refs.insightDepartChart.renderChart(newData, this.optionsInsightDepart);
         }
     },
     data() {
@@ -75,7 +140,100 @@ export default {
                     label: "JNE Main Page",
                     url: "https://core.jne.co.id/"
                 }
-            ]
+            ],
+            timeOfDay: "",
+            currentTime: "",
+            dataInsightInventory: {},
+            optionsInsightInventory: {
+                responsive: true,
+                maintainAspectRatio: false, 
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        grid: {
+                            color: '#e0e0e0',
+                            lineWidth: 1,
+                        },
+                        ticks: {
+                            color: '#333',
+                            font: {
+                                size: 12,
+                                family: 'Arial',
+                            },
+                        },
+                    },
+                    x: {
+                        grid: {
+                            display: false,
+                        },
+                        ticks: {
+                            color: '#333',
+                            font: {
+                                size: 12,
+                                family: 'Arial',
+                            },
+                        },
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                    tooltip: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                        titleColor: '#fff',
+                        bodyColor: '#fff',
+                        borderColor: '#1E88E5',
+                        borderWidth: 1,
+                    },
+                },
+            },
+            dataInsightRunsheet: {},
+            optionsInsightRunsheet: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Doughnut Chart Example', // Set your title here
+                        font: {
+                            size: 16,
+                            family: 'Arial',
+                            weight: 'bold',
+                        },
+                        color: '#333',
+                    },
+                    tooltip: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                        titleColor: '#fff',
+                        bodyColor: '#fff',
+                    },
+                },
+            },
+            dataInsightReceive: {},
+            optionsInsightReceive: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+            dataInsightDepart: {},
+            optionsInsightDepart: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                    },
+                },
+            },
         };
     },
     methods: {
@@ -108,7 +266,120 @@ export default {
             } else {
                 return "Evening";
             }
-        }
+        },
+        async getInsightInventory(){
+            this.loading = true;
+            try {
+                const res = await axios.get(`${this.URL.insight_inventory}?n=${this.listenNodeId}`, this.Helper.header());
+
+                if (res.data.data) {
+                    const keys = Object.keys(res.data.data);
+                    const values = Object.values(res.data.data);
+                    this.dataInsightInventory = {
+                        labels: keys,
+                        datasets: [{
+                            label: 'Count',
+                            backgroundColor: '#42A5F5',
+                            borderColor: '#1E88E5',
+                            data: values,
+                        }]
+                    };
+                }
+
+                console.log("P1", res.data.data, this.dataInsightInventory)
+                
+            } catch (err) {
+                this.openNotification('danger', err?.response?.data?.code || '', 'Failed', err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async getInsightRunsheet(){
+            this.loading = true;
+            try {
+                const res = await axios.get(`${this.URL.insight_runsheet}?n=${this.listenNodeId}`, this.Helper.header());
+
+                if (res.data.data) {
+                    const keys = Object.keys(res.data.data);
+                    const values = Object.values(res.data.data);
+                    this.dataInsightRunsheet = {
+                        labels: keys,
+                        datasets: [{
+                            backgroundColor: [
+                                '#FF6384',
+                                '#36A2EB',
+                                '#FFCE56'
+                            ],
+                            hoverBackgroundColor: [
+                                '#FF6384',
+                                '#36A2EB',
+                                '#FFCE56'
+                            ],
+                            data: values,
+                        }]
+                    };
+                }
+
+                console.log("P2", res.data.data, this.dataInsightRunsheet)
+                
+            } catch (err) {
+                this.openNotification('danger', err?.response?.data?.code || '', 'Failed', err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async getInsightReceive(){
+            this.loading = true;
+            try {
+                const res = await axios.get(`${this.URL.insight_receive}?n=${this.listenNodeId}`, this.Helper.header());
+
+                if (res.data.data) {
+                    const keys = Object.keys(res.data.data[0]);
+                    const values = Object.values(res.data.data[0]);
+                    this.dataInsightReceive = {
+                        labels: keys,
+                        datasets: [{
+                            label: 'Counts',
+                            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                            data: values,
+                        }]
+                    };
+                }
+
+                console.log("P3", res.data.data, this.dataInsightReceive)
+                
+            } catch (err) {
+                this.openNotification('danger', err?.response?.data?.code || '', 'Failed', err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async getInsightDepart(){
+            this.loading = true;
+            try {
+                const res = await axios.get(`${this.URL.insight_depart}?n=${this.listenNodeId}`, this.Helper.header());
+
+                if (res.data.data) {
+                    const keys = Object.keys(res.data.data[0]);
+                    const values = Object.values(res.data.data[0]);
+                    this.dataInsightDepart = {
+                        labels: keys,
+                        datasets: [{
+                            label: 'Counts',
+                            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                            data: values,
+                        }]
+                    };
+                }
+
+                console.log("P4", res.data.data, this.dataInsightDepart)
+                
+            } catch (err) {
+                this.openNotification('danger', err?.response?.data?.code || '', 'Failed', err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
     },
     mounted() {
         this.updateTime();
@@ -116,6 +387,22 @@ export default {
         if (this.listenUser) {
             this.getInfo(this.listenUser);
         }
+
+        this.getInsightInventory().then(() => {
+            this.$refs.insightInventoryChart.renderChart(this.dataInsightInventory, this.optionsInsightInventory);
+        });
+
+        this.getInsightRunsheet().then(() => {
+            this.$refs.insightRunsheetChart.renderChart(this.dataInsightRunsheet, this.optionsInsightRunsheet);
+        });
+
+        this.getInsightReceive().then(() => {
+            this.$refs.insightReceiveChart.renderChart(this.dataInsightReceive, this.optionsInsightReceive);
+        });
+
+        this.getInsightDepart().then(() => {
+            this.$refs.insightDepartChart.renderChart(this.dataInsightDepart, this.optionsInsightDepart);
+        });
     },
 };
 </script>
