2541f4e5a6cdb85032c24c911554dc763266af79
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-10-18 14:18:59 +0700

    adjust title for tracing

--- src/views/tracing/index.vue
+++ src/views/tracing/index.vue
@@ -4,7 +4,7 @@
             <vs-col xs="6" sm="4" lg="4">
                 <div class="titlePage">
                     <breadcrumb />
-                    <h2>{{ title }} {{  is_history ? "History" : "Outstanding" }}</h2>
+                    <h2>{{  is_history ? "Archive" : "Entry & Inventory" }}</h2>
                 </div>                
             </vs-col>
         </vs-row>
@@ -177,7 +177,6 @@ export default {
     },
     data() {
         return {
-            title:"Tracing",
             tempSearch: "",
             loading:false,
             dateRange: [],
