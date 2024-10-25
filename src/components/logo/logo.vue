211be75a5472a3bfc1e1ab0005b973a8fc1b5754
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-10-25 10:51:12 +0700

    add clickable jne logo

--- src/components/logo/logo.vue
+++ src/components/logo/logo.vue
@@ -1,5 +1,11 @@
 <template>
-    <img class="logo" :src="require('../../assets/img/jne.jpg')" alt="jne">
+    <a href="https://www.jne.co.id/" target="_blank" rel="noopener noreferrer">
+        <img 
+            :src="require('../../assets/img/jne.jpg')" 
+            class="logo" 
+            alt="jne"
+        >
+    </a>
 </template>
 <script>
 export default {
