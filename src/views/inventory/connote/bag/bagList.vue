a8e5e18dca52dad5c17c12f88538f77f85d06032
sautmanurung2 <saut.manurung@jne.co.id>
2024-10-28 19:24:25 +0700

    update path from - to be ~

--- src/views/inventory/connote/bag/bagList.vue
+++ src/views/inventory/connote/bag/bagList.vue
@@ -304,7 +304,7 @@ export default {
                 })
         },
         actionDetail(val){
-            let bag = val.bag_number.replaceAll("/", "-")
+            let bag = val.bag_number.replaceAll("/", "~")
             this.$router.push('/bagging-detail/'+bag)
             this.setRoutePageHistory(this.$route.meta, false);
         },
