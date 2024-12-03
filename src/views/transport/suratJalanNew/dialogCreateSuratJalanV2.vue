707108c260c0033a32ddfcc1af58167e18e28f83
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2024-12-04 06:59:46 +0700

    feat: close modal after success approve sj

--- src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
+++ src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
@@ -601,16 +601,18 @@ export default {
             else if (this.master_form.pic_employee_id === null) {
                 this.openNotification("warning", null, "Warning", "Please choose driver");
             } else {
+                this.loading = true;
                 try {
                     const res = await axios.patch(`${this.URL.revamp_surat_jalan_v2}/${this.manifest_do_number}/approval?n=${this.listenNodeId}`, {is_approve: this.is_approve ^ 1}, this.Helper.header());
                     
                     this.is_approve ^= 1;
                     this.isDisabled = !this.isDisabled;
+                    this.cancel();
                     this.openNotification("success", null, "Success", res?.data?.message);
                 } catch (err) {
                     this.openNotification("danger", err?.response?.data?.code ?? "", "Failed", err?.response?.data?.message ?? "Something went wrong"); 
                 } finally {
-                    this.$emit('refresh');
+                    this.loading = false;
                 }
             }
         },
