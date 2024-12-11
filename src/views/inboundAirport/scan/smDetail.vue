adb1cda492870c9da59bfb84046abac5a0b86b60
MuhammadTelaga <muh.telaga@jne.co.id>
2024-12-12 00:46:47 +0700

    update: adjustment inbound airport handle scan SM

---
+++ src/views/inboundAirport/scan/smDetail.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <table-master 
+            :dataTable="dataTable" 
+            :dataColumn="datacolumn" 
+            :tableLoading="listenLoading"
+            :removeDanger="false"
+            :hasAction="false"
+            :hasPagination="false"
+            :pageSize="pageSize"
+            :page="page"
+            :limit="limit"
+        />
+    </div>
+</template>
+<script>
+import master from "@/mixins/master"
+import TableMaster from "@/components/table/tableMaster.vue"
+export default {
+    name:"inbound-airport-scan-sm-table",
+    mixins: [master],
+    props: {
+        dataTable: Array,
+        loading: Boolean,
+        pageSize: Number,
+        page: Number,
+        limit: Number,
+        actionLimit: Function,
+        actionPagination: Function,
+    },
+    components: {
+        "table-master" : TableMaster
+    },
+
+    data() {
+        return {
+            datacolumn: [
+                {
+                    label: "Item Number",
+                    key: "item_number",
+                    width: "xxxxs"
+                },
+                {
+                    label: "Item type",
+                    key: "item_type",
+                    width: "xxxxs"
+                },
+                {
+                    label: "Item type",
+                    key: "item_type",
+                    width: "xxs"
+                },
+                {
+                  label: "Status receiving",
+                  key: "is_received",
+                  type: 'status',
+                  width: "xxs"
+                },
+            ],
+        }
+    },
+    computed: {
+        listenLoading(){
+            return this.loading
+        }
+    }
+}
+</script>
