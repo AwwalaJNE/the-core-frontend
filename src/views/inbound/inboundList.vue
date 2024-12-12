dc3948ccaf7ab6aae936d32201ff1480e24e6616
AlifahJNE <alifah.rahmatika@jne.co.id>
2024-12-12 12:02:31 +0700

    feat: add create in receiving date filter

--- src/views/inbound/inboundList.vue
+++ src/views/inbound/inboundList.vue
@@ -92,6 +92,11 @@ export default {
           width: "xs",
         },
         {
+          label: "Create",
+          key: "created_at",
+          width: "xs",
+        },
+        {
           label: "ETD",
           key: "inbound_etd",
           width: "xs",
@@ -362,9 +367,11 @@ export default {
           this.dataTable = res.data.data;
           this.dataTable.map((item) => {
             let im = [];
+            item["created_at"] = this.dateConvert(item["created_at"]);
             item["inbound_eta"] = this.dateConvert(item["inbound_eta"]);
             item["inbound_etd"] = this.dateConvert(item["inbound_etd"]);
             item["departed_at"] = this.dateConvert(item["departed_at"]);
+            item["received_at"] = this.dateConvert(item["received_at"]);
             item["vehicle"] = item["vehicle_type_name"];
             // item['is_prealert'] = isPrealert
             item["inbound_number"] =
--- src/views/inbound/index.vue
+++ src/views/inbound/index.vue
@@ -357,6 +357,10 @@ export default {
               {
                 label: 'Departed Time',
                 value: 'departed'
+              },
+              {
+                label: 'Create Date',
+                value: 'created'
               }
             ],
             reset: false,
