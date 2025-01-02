4b397a94a771b480dc7bd5f22c9761826aa610e4
MuhammadTelaga <muh.telaga@jne.co.id>
2025-01-02 14:03:06 +0700

    update: fix inner bags missing SM number in inventory bag data table

--- src/views/inventory/connote/bag/bagList.vue
+++ src/views/inventory/connote/bag/bagList.vue
@@ -383,7 +383,8 @@ export default {
                             })
                         }
 
-                        el.surat_muatan = el.surat_muatan.join(", ")
+                        // el.surat_muatan = el.surat_muatan.join(", ")
+                        el.surat_muatan = el.manifest_numbers
                         el.surat_jalan = el.surat_jalan.join(", ")
                         el.with_courier = el.courier ? el.courier.employee_name : ""
                         el.approved = el.is_approve === 1 ? "Yes" : "No"
