07cf68493148b2bf116a7046fb3da12bbffd43ad
MuhammadTelaga <muh.telaga@jne.co.id>
2025-01-08 13:34:34 +0700

    update: modify column weight in SM

--- src/views/transport/manifestNew/manifest.vue
+++ src/views/transport/manifestNew/manifest.vue
@@ -109,14 +109,24 @@ export default {
                         width: "auto"
                     },
                     {
-                        label: "Cost Weight(Kg)",
-                        key: "cost_weight",
-                        width: "auto"
+                        label: "Fix Cost Weight",
+                        key: "fix_cost_weight",
+                        width: "auto",
                     },
                     {
-                        label: "Actual Weight(Kg)",
-                        key: "actual_weight",
-                        width: "auto"
+                        label: "Live Cost Weight",
+                        key: "live_cost_weight",
+                        width: "auto",
+                    },
+                    {
+                        label: "Fix Actual Weight",
+                        key: "fix_actual_weight",
+                        width: "auto",
+                    },
+                    {
+                        label: "Live Actual Weight",
+                        key: "live_actual_weight",
+                        width: "auto",
                     },
                     {
                         label: "ETD",
