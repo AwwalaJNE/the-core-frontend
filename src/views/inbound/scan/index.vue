69f5a0a569d5efbd2b41cd539bae578a2c5ecbcc
AlifahJNE <alifah.rahmatika@jne.co.id>
2025-01-10 14:28:22 +0700

    feat: remove prealert lock in receiving

--- src/views/inbound/scan/index.vue
+++ src/views/inbound/scan/index.vue
@@ -265,10 +265,6 @@ export default {
                 this.openNotification('success', null, "Success", res?.data?.message ?? "Receiving success");
                 this.inbound_number = res?.data?.data?.inbound_number ?? this.inbound_number;
                 this.hasInboundNumber = true;
-                let prealert = ['SM', 'SJ', 'PICKUP'];
-                if (prealert.includes(res?.data?.data?.inbound_type)) {
-                    this.parent_no = this.inbound_number;
-                }
                 this.refresh();
             } catch (err) {
                 this.openNotification("danger", err?.response?.data?.code ?? '', "Failed", err?.response?.data?.message ?? 'Something went wrong');
