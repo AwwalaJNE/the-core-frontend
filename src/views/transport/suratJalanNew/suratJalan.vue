58a19380aa719ca43594f6dcd0421ae3009f20c0
Honestyan Didyafarhan <honestyan0708@gmail.com>
2025-02-03 11:04:33 +0700

    feat(suratJalanNew): update items mapping logic for auto-filled fields

--- src/views/transport/suratJalanNew/suratJalan.vue
+++ src/views/transport/suratJalanNew/suratJalan.vue
@@ -323,12 +323,17 @@ export default {
             item["orion_number"] = item.mts || item.do || "";
             item["approved"] = item.is_approve === 1 ? "YES" : "NO";
 
-            if (item.manifest_do_number?.startsWith("SJA") || item.manifest_do_number?.startsWith("BM")) {
-              item['orion_number'] = 'Auto By System'
-              item['vehicle_type_name'] = 'Auto By System'
-              item['driver_name'] = 'Auto By System',
-              item['vehicle_mode_name'] = 'Auto By System'
-            }
+            if (
+                  (item.manifest_do_number?.startsWith("SJA") ||
+                  item.manifest_do_number?.startsWith("BM")) &&
+                  item['driver_name'] === null &&
+                  item['vehicle_mode_name'] === null
+                ) {
+                  item['orion_number'] = 'Auto By System';
+                  item['vehicle_type_name'] = 'Auto By System';
+                  item['driver_name'] = 'Auto By System';
+                  item['vehicle_mode_name'] = 'Auto By System';
+                }
 
             if (item.hasOwnProperty("status") && item["status"] !== null) {
               let str = item["status"].toLowerCase();
