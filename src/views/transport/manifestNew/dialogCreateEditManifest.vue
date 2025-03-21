629a65d3633eb5e74ffa35308d97095baaaf40c0
MuhammadTelaga <muh.telaga@jne.co.id>
2025-03-21 12:58:25 +0700

    hotfix: css error

--- src/views/transport/manifestNew/dialogCreateEditManifest.vue
+++ src/views/transport/manifestNew/dialogCreateEditManifest.vue
@@ -1,10 +1,10 @@
 <template>
     <dialog-master
-        width="xl"
+        width="lg"
         :actived="listenActive"
         :loading="listenLoading"
         :closeDialog="cancel"
-        class="custom-width-manifest"
+        class="custom-width"
     >
         <template v-slot:header>
             <div class="button-helper">
@@ -868,7 +868,7 @@ export default {
 </script>
 <style scoped>
 .title-helper {
-    width: 73%;
+    width: 60%;
     align-content: center;
 }
 
@@ -880,20 +880,4 @@ export default {
 button {
     width: 6em;
 }
-
-::v-deep(.vs-dialog-content.xl) {
-    width: 95vw !important;
-    max-width: 95vw !important;
-}
-
-::v-deep(.vs-dialog) {
-    width: 95vw !important;
-    max-width: 95vw !important;
-}
-
-::v-deep(table) {
-    width: 100%;
-    min-width: 1200px;
-}
-
 </style>
