5012f4689d8351c24ddb6b0baaa27eb990fd6aec
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-04-22 15:42:10 +0700

    feat: adjust disable handler for destination switch

--- src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
+++ src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
@@ -305,6 +305,8 @@ export default {
             this.dataTable = val.detail;
             this.is_penerusan = val.is_penerusan === "1";
 
+            this.isDestinationEnabled = val.node_id_destination === null;
+
             this.isDisabled = val.status !== 'READY' || val.is_orion === "1" || val.is_approve === 1;
             this.isDisabledPrint = val.status === 'CANCELED';
             this.isDisabledApprove = val.status !== 'READY' || val.is_orion === "1";
@@ -705,20 +707,15 @@ export default {
                             arr.push(obj);
                         });
 
-                        this.nodeDestination = arr
-                        if (!this.manifest_do_number || this.dataItem.node_id_destination) {
-                            this.$store.dispatch(
-                                "SET_SURAT_JALAN_DESTINATION_ID_ArrData",
-                                arr.length > 0 ? arr : null
-                            );
-                        }
+                        this.$store.dispatch(
+                            "SET_SURAT_JALAN_DESTINATION_ID_ArrData",
+                            arr.length > 0 ? arr : null
+                        );
                     } else {
-                        if (!this.manifest_do_number || this.dataItem.node_id_destination) {
-                            this.$store.dispatch(
-                                "SET_SURAT_JALAN_DESTINATION_ID_ArrData",
-                                null
-                            );
-                        }
+                        this.$store.dispatch(
+                            "SET_SURAT_JALAN_DESTINATION_ID_ArrData",
+                            null
+                        );
                     }
                 })
                 .catch((err) => {
