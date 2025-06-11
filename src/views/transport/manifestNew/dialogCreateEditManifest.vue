d7d71f4ed9631ec71ec4b89a7a5c57f80c1c4c5c
MuhammadTelaga <muh.telaga@jne.co.id>
2025-06-12 00:59:46 +0700

    create missroute received icon to bag detail SJ and SM

--- src/views/transport/manifestNew/dialogCreateEditManifest.vue
+++ src/views/transport/manifestNew/dialogCreateEditManifest.vue
@@ -203,6 +203,7 @@ export default {
                 {
                     label: "Received",
                     key: "received_status",
+                    is_missroute: "is_missroute",
                     type: "status",
                     width: "xs",
                 },
@@ -242,6 +243,7 @@ export default {
             master_form: {},
             dataByApi: {},
             loadingSuratMuatan: false,
+            isMissroute: false
         };
     },
     computed: {
@@ -312,6 +314,7 @@ export default {
 
             this.vehicle_mode_id = val.vehicle_mode_id;
             this.vehicle_type_id = val?.vehicle_type_id ?? null;
+            this.is_missroute = val.is_missroute === true ? 1 : 0
 
             if (this.vehicle_mode_id) {
                 this.getDataVehicleType();
@@ -364,6 +367,11 @@ export default {
 
                 this.dataTable = arr;
             }
+
+            console.log('dataTable', this.dataTable);
+            this.dataTable.forEach(item => {
+                item.is_missroute = this.is_missroute
+            })
 
             this.master_form = {
                 manifest_number: val.manifest_number,
@@ -734,7 +742,8 @@ export default {
                     arr = arr.map(item => ({
                         ...item,
                         received_status: item.received_at ? 1 : 0,
-                        destination_name: item.item_destination
+                        destination_name: item.item_destination,
+                        is_missroute: item.is_missroute
                     }));
 
                     this.dataTable = arr;
--- src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
+++ src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
@@ -217,6 +217,7 @@ export default {
                 {
                     label: "Received",
                     key: "received_status",
+                    is_missroute: "is_missroute",
                     type: "status",
                     width: "sm",
                 },
@@ -261,7 +262,8 @@ export default {
                 page: 1,
             },
             editData: {},
-            destination_name_code: ''
+            destination_name_code: '',
+            is_missroute:false
         };
     },
     computed: {
@@ -306,6 +308,7 @@ export default {
             this.isDisabledApprove = val.status !== 'UNAPPROVED' || val.is_orion === "1";
 
             this.is_approve = val.is_approve;
+            this.is_missroute = val.is_missroute === true ? 1 : 0
 
             this.dataTable.forEach(item => {
                 item.destination = item.bag?.destination?.node_tariff_code || item.koli?.connote?.connote_receiver_tariff_code || item.manifest?.destination?.node_tariff_code || '';
@@ -317,6 +320,7 @@ export default {
                 }
 
                 item.received_status = item.received_at ? 1 : 0;
+                item.is_missroute = this.is_missroute
             });
 
             this.total_weight = val.total_weight;
@@ -580,7 +584,8 @@ export default {
                         ...item,
                         received_status: item.received_at ? 1 : 0,
                         destination: item.item_destination,
-                        node_code_destination: item.node_code_destination
+                        node_code_destination: item.node_code_destination,
+                        is_missroute: item.is_missroute
                     }));
 
                     this.dataTable = arr;
