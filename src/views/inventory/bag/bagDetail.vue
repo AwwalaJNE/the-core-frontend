dd838cfe13a731750d67366bb5d0c3357d6a860e
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-08-20 14:56:01 +0700

    feat: init transit bag dialog

--- src/views/inventory/bag/bagDetail.vue
+++ src/views/inventory/bag/bagDetail.vue
@@ -15,7 +15,7 @@
         </div>
       </div>
       <div style="display: flex;" class="buttonPage" v-if="!loading">
-        <vs-button v-if="listenUserRoleName !== 'HELPDESK' && listenDataBag.tipe_bag === 'OM'" @click="newBag">
+        <vs-button v-if="listenUserRoleName !== 'HELPDESK'" @click="openDialogTransit">
           <i class="bx bx-plus"></i> Add Transit
         </vs-button>
         <template v-if="listenUserRoleName === 'HELPDESK'">
@@ -338,6 +338,13 @@
       :bagNumber="bag_id"
       :closeDialog="closeDialog"
     />
+
+    <dialog-create-edit-transit
+      title="Add Transit Route"
+      :active="dialogTransitActive"
+      :bagNumber="bag_id"
+      :closeDialog="closeDialogTransit"
+    />
   </div>
 </template>
 <script>
@@ -349,6 +356,7 @@ import Selector from "@/components/input/select"
 import CameraScanner from "@/components/scanner/camera.vue";
 import DialogConfirm from "@/components/dialog/dialogConfirm"
 import DialogHelpdeskEditBag from "@/views/helpdesk/bag/dialogHelpdeskEditBag";
+import DialogCreateEditTransit from "@/views/inventory/bag/transit/dialogCreateEdit.vue";
 
 import asynchronousSelect from "@/components/input/asynchronousSelect"
 
@@ -362,6 +370,7 @@ export default {
     CameraScanner,
     "dialog-confirm": DialogConfirm,
     "dialog-helpdesk-edit-bag": DialogHelpdeskEditBag,
+    "dialog-create-edit-transit": DialogCreateEditTransit,
     "asynchronousSelect": asynchronousSelect,
   },
   data() {
@@ -416,7 +425,8 @@ export default {
       disable_hub_delivery: false,
       validation: '',
       validation_reference: [], 
-      courierArr: []
+      courierArr: [],
+      dialogTransitActive: false,
     }
   },
   computed: {
@@ -728,6 +738,12 @@ export default {
       this.$router.push('/outgoing/bag')
       this.setRoutePageHistory(this.$route.meta, false);
       
+    },
+    openDialogTransit() {
+      this.dialogTransitActive = true;
+    },
+    closeDialogTransit() {
+      this.dialogTransitActive = false;
     },
     editBag() {
       this.dialogHelpdeskEditBag = true;
---
+++ src/views/inventory/bag/transit/dialogCreateEdit.vue
@@ -0,0 +1,190 @@
+<template>
+    <div>
+        <dialog-master 
+            width="lg"
+            :actived="listenActive" 
+            :closeDialog="cancel"
+            :loading="listenLoading"
+        >
+            <template v-slot:header>
+                {{listenTitle}}
+            </template>
+
+            <template v-slot:content>
+                <vs-row align="center">
+                    <vs-col w="10">
+                        <auto-complete
+                            formKey="origin_code"
+                            name="Origin Code"
+                            typeInput="autocomplete"
+                            :querySearch="querySearch"
+                            @updateValue="updateValue"
+                        />
+                    </vs-col>
+                    <vs-col w="2">
+                        <vs-button
+                            block
+                            flat
+                            transparent
+                            type="submit"
+                            :active="true"
+                            @click="createTransit"
+                        >
+                            <i class="bx bx-plus"></i> Add
+                        </vs-button>
+                    </vs-col>
+                </vs-row>
+                <vs-row justify="center">
+                    <template v-if="data.length > 0">
+                        <div>
+                            haii
+                        </div>
+                    </template>
+                    <template v-else>
+                        <div style="padding: 2rem;">
+                            <img 
+                                src="@/assets/svg/not-found-transit.svg" 
+                                alt="No Transit Route Found"
+                            />
+                        </div>
+                    </template>
+                </vs-row>
+            </template>
+        </dialog-master>
+    </div>
+</template>
+<script>
+import axios from "axios";
+import master from "@/mixins/master";
+
+import AutoComplete from "@/components/input/autoComplete";
+import DialogMaster from "@/components/dialog/dialogMaster";
+
+export default {
+    name:"transit-dialog",
+    mixins: [master],
+    components: {
+        "auto-complete": AutoComplete,
+        "dialog-master": DialogMaster,
+    },
+    props: {
+        active: Boolean,
+        bagNumber: String,
+        closeDialog: Function,
+        title: String
+    },
+    data() {
+        return {
+            data: [],
+            form: {},
+            form_order: {},
+            loading: false,
+        }
+    },
+    computed: {
+        listenActive(){
+            return this.active;
+        },
+        listenTitle(){
+            return this.title;
+        },
+        listenLoading() {
+            return this.loading;
+        },
+    },
+    watch: {
+        active(newVal, oldVal) {
+            if (newVal === true && newVal !== oldVal) {
+                this.getDataTransit();
+            }
+        }
+    },
+    methods: {
+        async getDataTransit() {
+            this.loading = true
+            
+            try {
+                const res = await axios.get(`${this.URL.bag}/${this.bagNumber}/route-transit?n=${this.listenNodeId}`, this.Helper.header());
+                this.data = res?.data?.data || [];
+            } catch (err) {
+                this.openNotification('danger', err?.response?.data?.code || '', 'Failed', err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async createTransit() {
+            if (Object.keys(this.form).length === 0) {
+                this.openNotification('warn', null, "Missing Origin Code", "Please select an origin code to proceed with this action");
+                return;
+            }
+
+            this.loading = true;
+            try {
+                const res = await axios.post(`${this.URL.bag}/${this.bagNumber}/route-transit?n=${this.listenNodeId}`, this.form, this.Helper.header());
+                this.openNotification('success', null, "Success", res?.data?.message || "Success Add Transit Data");
+            } catch (err) {
+                this.openNotification("danger", err?.response?.data?.code || '', "Failed", err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+                this.getDataTransit();
+            }
+        },
+        async updateTransit () {
+            this.loading = true;
+            try {
+                const res = await axios.patch(`${this.URL.bag}/${this.bagNumber}/route-transit?n=${this.listenNodeId}`, this.form_order, this.Helper.header());
+                this.openNotification('success', null, "Success", "Update manifest vehicle success");
+
+                await this.getManifestVehicle();
+            } catch (err) {
+                this.openNotification("danger", err?.response?.data?.code || '', "Failed", err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async removeTransit(id) {
+            this.loading = true;
+            try {
+                const res = await axios.delete(`${this.URL.bag}/${this.bagNumber}/route-transit/${id}?n=${this.listenNodeId}`, this.Helper.header());
+                this.openNotification('success', null, "Success", "Remove transit success");
+            } catch (err) {
+                this.openNotification("danger", err?.response?.data?.code || '', "Failed", err?.response?.data?.message || 'Something went wrong');
+            } finally {
+                this.loading = false;
+            }
+        },
+        async querySearch(queryString, cb){
+            try {
+                const res = await axios.get(`${this.URL.origin_code}?n=${this.listenNodeId}` + `&s=${queryString}`, this.Helper.header());
+                const result = res.data.data || [];
+                const suggestions = result.map(item => {
+                    const value = item.origin_code || '';
+                    return { value, data: item };
+                });
+                cb(suggestions);
+            } catch (error) {
+                console.error("error", error);
+            }
+        },
+        updateValue(key, val, info){
+            switch(key) {
+                case "origin_code":
+                    this.form = info?.data;
+                    break;
+            }
+        },
+        handleClear() {
+            this.data = [];
+            this.form = {};
+            this.form_order = {};
+        },
+        cancel() {
+            this.handleClear();
+            this.closeDialog();
+        },
+    },
+    mounted() {
+        this.handleSubmitShortcut(this.handleSubmit)
+    },
+}
+</script>
