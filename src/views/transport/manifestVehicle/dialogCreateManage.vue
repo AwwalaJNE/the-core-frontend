a5479ee3c1e1778be4f96610ddb5db090c642ddb
MuhammadTelaga <muh.telaga@jne.co.id>
2025-08-25 15:22:18 +0700

    add validation for manual manifest vehicle creation

--- src/views/transport/manifestVehicle/dialogCreateManage.vue
+++ src/views/transport/manifestVehicle/dialogCreateManage.vue
@@ -296,7 +296,16 @@ export default {
                         eta_vehicle: form.eta
                     };
                 }
-                
+
+                if (data.pic_employee_id === '') {
+                    this.openNotification('warning', '', 'Failed', 'Driver belum dipilih. Silahkan pilih driver terlebih dahulu');
+                    return
+                }
+                if (data.etd_vehicle > data.eta_vehicle) {
+                    this.openNotification('warning', '', 'Failed', 'Tanggal ETD tidak boleh lebih besar dari Tanggal ETA');
+                    return
+                }
+
                 this.$emit('updateVehicleValue', data);
                 this.cancel();
             }
