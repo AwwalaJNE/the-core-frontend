a19090dcca652fa23d8356009d749a69f6cb55df
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-09-19 14:20:39 +0700

    feat: show only value for data

--- src/components/input/select.vue
+++ src/components/input/select.vue
@@ -105,7 +105,7 @@
                                 v-for="(item, key) in DataArr"
                                 :key="key"
                                 :value="item.value"
-                                :label="!item.hasOwnProperty('formattedLabel') && item.label"
+                                :label="!item.hasOwnProperty('formattedLabel') && (item.code || item.label)"
                             >
                                 <template v-if="item.hasOwnProperty('formattedLabel')">
                                 <span v-for="(line, index) in item.formattedLabel" :key="index">
