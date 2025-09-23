57eb04ba7a9a1ef8e34a3ad190d5e293bcbaf6fd
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-09-23 17:35:26 +0700

    feat: add refresh while change timezone in sm stock list

--- src/views/settings/suratMuatan/stock/index.vue
+++ src/views/settings/suratMuatan/stock/index.vue
@@ -461,7 +461,11 @@ export default {
             }
         },
     },
+    beforeDestroy() {
+        window.removeEventListener('timezone-changed', this.refresh);
+    },
     mounted() {
+        window.addEventListener('timezone-changed', this.refresh);
         this.refresh();
         this.getVehicleMode();
     },
