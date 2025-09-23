355f8004c3081c2cb8fca9fd3dffadb627180042
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-09-23 18:19:02 +0700

    feat: add format to wib in get manifest vehicle in sm stock

--- src/views/transport/manifestNew/dialogCreateEditManifest.vue
+++ src/views/transport/manifestNew/dialogCreateEditManifest.vue
@@ -915,9 +915,9 @@ export default {
                     vehicle_id: active_vehicle?.vehicle_name || "",
                     pic_employee_id: active_vehicle?.pic_employee_id || "",
                     flight_number: active_vehicle?.flight_number || "",
-                    flight_schedule: active_vehicle?.etd || "",
-                    etd_vehicle: active_vehicle?.etd || "",
-                    eta_vehicle: active_vehicle?.eta || "",
+                    flight_schedule: this.formatTimezone(active_vehicle?.etd) || "",
+                    etd_vehicle: this.formatTimezone(active_vehicle?.etd) || "",
+                    eta_vehicle: this.formatTimezone(active_vehicle?.eta) || "",
                     status_flight: active_vehicle?.status_flight,
                     is_active: active_vehicle?.status === 'ACTIVE'
                 }];
