08ff390d1094d1702d372fde96b911a6f0855508
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-09-24 16:57:15 +0700

    fix: handle format date time for date only

--- src/mixins/master.js
+++ src/mixins/master.js
@@ -625,10 +625,14 @@ const Master = {
                 // format ISO → langsung parse sebagai UTC
                 utcDate = new Date(date);
             } else {
-                // format manual "YYYY-MM-DD HH:mm:ss"
+                // format manual "YYYY-MM-DD" atau "YYYY-MM-DD HH:mm:ss"
                 const [datePart, timePart] = date.split(" ");
                 const [year, month, day] = datePart.split("-").map(Number);
-                const [hour, minute, second] = timePart.split(":").map(Number);
+
+                // kalau ga ada time, default jam 00:00:00
+                const [hour, minute, second] = timePart
+                    ? timePart.split(":").map(Number)
+                    : [0, 0, 0];
 
                 const baseDate = new Date(year, month - 1, day, hour, minute, second);
 
