575825a903634979543ce4b19eaea7e429fafe0c
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-10-29 18:00:42 +0700

    feat: implement new api validation item for scan bag

--- src/config.js
+++ src/config.js
@@ -13,230 +13,229 @@ export default {
     app_jne_core_dashboard,
     app_jne_main,
 
-    login: api + "login",
-    user_auth_data: api + "user-auth-data",
-    user: api + "user",
-    role: api + "role",
-    permission: api + "permission",
+    login: api + 'login',
+    user_auth_data: api + 'user-auth-data',
+    user: api + 'user',
+    role: api + 'role',
+    permission: api + 'permission',
 
     // geolocation
-    geolocation: api + "geolocation",
-    geolocation_country: api + "geolocation-country",
-    geolocation_city: api + "geolocation-city",
-    geolocation_district: api + "geolocation-district",
-    geolocation_subdistrict: api + "geolocation-subdistrict",
-    geolocation_province: api + "geolocation-province",
-    geolocation_timezone: api + "geolocation-timezone",
-    geolocation_search: api + "geolocation-search",
-    geolocation_search_by: api + "geolocation-search-by",
+    geolocation: api + 'geolocation',
+    geolocation_country: api + 'geolocation-country',
+    geolocation_city: api + 'geolocation-city',
+    geolocation_district: api + 'geolocation-district',
+    geolocation_subdistrict: api + 'geolocation-subdistrict',
+    geolocation_province: api + 'geolocation-province',
+    geolocation_timezone: api + 'geolocation-timezone',
+    geolocation_search: api + 'geolocation-search',
+    geolocation_search_by: api + 'geolocation-search-by',
 
     // node
-    node: api + "node",
-    node_commission: api + "node-commission",
-    node_link: api + "node-link",
-    node_type: api + "node-type",
-    node_alternate_address: api + "node-alternate-address",
-    node_ip_address: api + "node-ip-address",
-    node_list: api + "node-list",
+    node: api + 'node',
+    node_commission: api + 'node-commission',
+    node_link: api + 'node-link',
+    node_type: api + 'node-type',
+    node_alternate_address: api + 'node-alternate-address',
+    node_ip_address: api + 'node-ip-address',
+    node_list: api + 'node-list',
 
     // employee
-    employee: api + "employee",
+    employee: api + 'employee',
 
     // employee type
-    employee_type: api + "employee-type",
+    employee_type: api + 'employee-type',
 
     // customer
-    customer: api + "customer",
-    customer_type: api + "customer-type",
-    search_customer_code: api + "search-customer-code",
+    customer: api + 'customer',
+    customer_type: api + 'customer-type',
+    search_customer_code: api + 'search-customer-code',
 
     // tariff
-    tariff: api + "tariff",
-    tariff_special: api + "tariff-special",
-    discount_tariff: api + "discount-tariff",
+    tariff: api + 'tariff',
+    tariff_special: api + 'tariff-special',
+    discount_tariff: api + 'discount-tariff',
 
     //surcharge
-    surcharge: api + "surcharge",
-    surcharge_type: api + "surcharge-type",
+    surcharge: api + 'surcharge',
+    surcharge_type: api + 'surcharge-type',
 
     //vehicle
-    vehicle: api + "vehicle",
-    vehicle_manifest_delivery_order: api + "vehicle-manifest-delivery-order",
-    vehicle_mode: api + "vehicle-mode",
-    vehicle_type: api + "vehicle-type",
-    pickup_vehicle_type: api + "pickup-vehicle-type",
+    vehicle: api + 'vehicle',
+    vehicle_manifest_delivery_order: api + 'vehicle-manifest-delivery-order',
+    vehicle_mode: api + 'vehicle-mode',
+    vehicle_type: api + 'vehicle-type',
+    pickup_vehicle_type: api + 'pickup-vehicle-type',
 
     // transaction
-    tariff_shipping_service: api + "tariff-shipping-service",
-    connote: api + "connote",
-    upload_connote: api + "upload-connote",
-    booking_connote: api + "booking-connote",
-    transaction: api + "transaction",
-    koli: api + "koli",
-    bag: api + "bag",
-    bag_inventory: api + "bag-inventory",
-    unbagging: api + "unbagging",
-    cash_register: api + "cash-register",
-    payment: api + "payment",
+    tariff_shipping_service: api + 'tariff-shipping-service',
+    connote: api + 'connote',
+    upload_connote: api + 'upload-connote',
+    booking_connote: api + 'booking-connote',
+    transaction: api + 'transaction',
+    koli: api + 'koli',
+    bag: api + 'bag',
+    bag_inventory: api + 'bag-inventory',
+    unbagging: api + 'unbagging',
+    cash_register: api + 'cash-register',
+    payment: api + 'payment',
 
     // Service
-    service: api + "service-list",
+    service: api + 'service-list',
 
     //  inventory Vehicle
-    inventory_vehicle: api + "inventory-vehicle",
+    inventory_vehicle: api + 'inventory-vehicle',
 
     // PRINT
-    print: api + "print",
+    print: api + 'print',
 
     //PICKUP
-    pickup: api + "pickup",
+    pickup: api + 'pickup',
 
     //PICKUP REQUEST
-    pickup_request: api + "pickup-request",
+    pickup_request: api + 'pickup-request',
 
     //PICKUP SCHEDULE
-    pickup_schedule: api + "pickup-schedule",
+    pickup_schedule: api + 'pickup-schedule',
 
     // PICKUP COURIER
-    pickup_courier: api + "pickup-courier",
-
+    pickup_courier: api + 'pickup-courier',
 
     //INBOUND
-    inbound: api + "inbound",
+    inbound: api + 'inbound',
 
     //INBOUND INCOMING
-    inbound_incoming: api + "inbound-incoming",
+    inbound_incoming: api + 'inbound-incoming',
 
     //INBOUND RECEIVING
-    receiving: api + "receiving",
+    receiving: api + 'receiving',
 
     //SURAT JALAN
-    manifest_delivery_order: api + "manifest-delivery-order",
-    manifest_delivery_order_airport: api + "manifest-delivery-order-airport",
+    manifest_delivery_order: api + 'manifest-delivery-order',
+    manifest_delivery_order_airport: api + 'manifest-delivery-order-airport',
 
     //delivery runsheet
-    delivery : api + "delivery",
+    delivery: api + 'delivery',
 
     //courier delivery
-    courier_delivery : api + "courier-delivery",
+    courier_delivery: api + 'courier-delivery',
 
     //delivery return
-    delivery_return : api + "delivery/return",
+    delivery_return: api + 'delivery/return',
 
     //delivery cod
-    delivery_cod : api + "cod",
+    delivery_cod: api + 'cod',
 
     //delivery cod collect
-    delivery_cod_collect : api + "cod-collect",
+    delivery_cod_collect: api + 'cod-collect',
 
     //delivery cod history
-    delivery_cod_history : api + "cod-history",
+    delivery_cod_history: api + 'cod-history',
 
     //SURAT MUATAN
-    surat_muatan: api + "manifest",
-    manifest_do: api + "manifest-do",
+    surat_muatan: api + 'manifest',
+    manifest_do: api + 'manifest-do',
 
     //display-list-status
-    status: api + "status",
+    status: api + 'status',
 
     //irregularities
-    irregularities: api + "irregularities",
-    return: api + "return",
+    irregularities: api + 'irregularities',
+    return: api + 'return',
 
     //tracing
-    tracing : api + "tracing",
+    tracing: api + 'tracing',
 
     //tracing_status
-    tracing_status : api + "tracing/status",
+    tracing_status: api + 'tracing/status',
 
     //tracing_warehouse
-    tracing_warehouse : api + "tracing/warehouse",
+    tracing_warehouse: api + 'tracing/warehouse',
 
     //cost to cost
-    cost_to_cost: api + "cost-to-cost",
+    cost_to_cost: api + 'cost-to-cost',
 
     //cost to cost
-    cost_to_cost_rules: api + "cost-to-cost-rules",
+    cost_to_cost_rules: api + 'cost-to-cost-rules',
 
     //cost to cost
-    cost_to_cost_type: api + "cost-to-cost-type",
+    cost_to_cost_type: api + 'cost-to-cost-type',
 
     //cost to cost Report
-    cost_to_cost_report: api + "cost-to-cost-report",
+    cost_to_cost_report: api + 'cost-to-cost-report',
 
     //cost to cost
-    activity_type: api + "activity-type",
+    activity_type: api + 'activity-type',
 
     //config
-    config: api + "config",
+    config: api + 'config',
 
     //packingkayu
-    packing_kayu: api + "packingkayu",
+    packing_kayu: api + 'packingkayu',
 
     //cashless
-    cashless: api + "cashless",
+    cashless: api + 'cashless',
 
     //profile
-    profile: api + "profile",
+    profile: api + 'profile',
 
     //reset-token
-    reset_token: api + "reset-token",
+    reset_token: api + 'reset-token',
 
     //apidoc
-    apidoc: urlFOrmated.origin + "/docs",
+    apidoc: urlFOrmated.origin + '/docs',
 
     //user_history
-    user_history: api + "auth-history-tracker",
+    user_history: api + 'auth-history-tracker',
 
     //pod
-    pod : api + "pod",
+    pod: api + 'pod',
 
     //undel
-    undelivery : api + "undelivery",
+    undelivery: api + 'undelivery',
 
     //Receiving Runsheet
-    receiving_runsheet : api + "receiving-runsheet",
+    receiving_runsheet: api + 'receiving-runsheet',
 
     //check-auth
-    check_auth : api + "check-auth",
+    check_auth: api + 'check-auth',
 
     //tariff-group
-    tariff_group : api + "tariff-group",
+    tariff_group: api + 'tariff-group',
 
     //pickup-origin
-    pickup_origin : api + "pickup-origin",
+    pickup_origin: api + 'pickup-origin',
 
     //resync runsheet
-    resync_runsheet : api + "resync/delivery",
+    resync_runsheet: api + 'resync/delivery',
 
     //origin code
-    origin_code : api + "origin-code",
+    origin_code: api + 'origin-code',
 
     //destination code
-    destination_code : api + "get-destination",
+    destination_code: api + 'get-destination',
 
     //sla
-    sla : api + "sla",
-    sla_koli : api + "sla/koli",
-    sla_inter_activity : api + "sla/inter-activity",
-    sla_node_to_node_a : api + "sla/node-to-node-a",
-    sla_node_to_node_b : api + "sla/node-to-node-b",
-    
+    sla: api + 'sla',
+    sla_koli: api + 'sla/koli',
+    sla_inter_activity: api + 'sla/inter-activity',
+    sla_node_to_node_a: api + 'sla/node-to-node-a',
+    sla_node_to_node_b: api + 'sla/node-to-node-b',
+
     // SLA Bag
-    sla_bag : api + "sla-bag",
+    sla_bag: api + 'sla-bag',
 
     // Configuration Warning Runsheet
-    configuration_warning_runsheet : api + 'warning-koli-setting',
-    configuration_warning_sla : api + 'warning-runsheet-sla-setting',
+    configuration_warning_runsheet: api + 'warning-koli-setting',
+    configuration_warning_sla: api + 'warning-runsheet-sla-setting',
 
     // Configuration Warning Runsheet
-    warning_runsheet : api + 'warning-koli-record',
+    warning_runsheet: api + 'warning-koli-record',
 
     // Check Delivery Area
-    check_delivery_area : api + 'check-delivery-area',
+    check_delivery_area: api + 'check-delivery-area',
 
     // Delivery Area
-    node_delivery_area : api + 'node-delivery-area',
+    node_delivery_area: api + 'node-delivery-area',
 
     // Documentation
     documentation: api + 'documentation',
@@ -245,7 +244,7 @@ export default {
     validation: api + 'validation',
 
     //Handover Runsheet
-    handover_runsheet : api + "handover-runsheet",
+    handover_runsheet: api + 'handover-runsheet',
 
     // Find Feature
     find_feature: api + 'find-feature',
@@ -358,7 +357,7 @@ export default {
 
     // Masterdata Runsheet Limit
     runsheet_limit_setting: api + 'runsheet-limit',
-  
+
     // Sorting Validation Zip Code
     sorting_zip_code_validation: api + 'validation/zip-code-new',
 
@@ -388,7 +387,7 @@ export default {
     // Vehicle Id V2
     vehicle_list_v2: api + 'vehicle/list',
 
-    // List SM 
+    // List SM
     list_sm: api + 'list/sm-number',
 
     // Receiving Log
@@ -445,5 +444,7 @@ export default {
     active_bag_weight: api + 'active-bag-weight',
 
     // user preferences
-    user_preferences: api + 'user-preferences'
+    user_preferences: api + 'user-preferences',
+
+    validation_item: api + 'validation/item',
 }
--- src/views/inventory/bag/index.vue
+++ src/views/inventory/bag/index.vue
@@ -222,7 +222,29 @@ export default {
             if (this.bag_type === 'pra runsheet') {
                 this.createBag()
             } else {
-                await this.processSorting()
+                await this.validateItem()
+            }
+        },
+        async validateItem() {
+            this.startLoading(this.$refs.baggingSection)
+            try {
+                const res = await axios.get(
+                    `${this.URL.validation_item}?n=${this.listenNodeId}`,
+                    this.Helper.header()
+                )
+
+                this.processSorting()
+
+                this.openNotification('success', null, 'Success', res?.data?.message || 'Success')
+            } catch (err) {
+                this.openNotification(
+                    'danger',
+                    err?.response?.data?.code || '',
+                    'Failed',
+                    err?.response?.data?.message || 'Something went wrong'
+                )
+            } finally {
+                this.stopLoading()
             }
         },
         async processSorting() {
@@ -242,16 +264,7 @@ export default {
 
                 this.openNotification('success', null, 'Success', res?.data?.message || 'Success')
             } catch (err) {
-                if (['CORE-1146', 'CORE-1082', 'CORE-1156'].includes(err?.response?.data?.code)) {
-                    this.openNotification(
-                        'danger',
-                        err?.response?.data?.code || '',
-                        'Failed',
-                        err?.response?.data?.message || 'Something went wrong'
-                    )
-                } else {
-                    this.openDialog()
-                }
+                this.openDialog()
             } finally {
                 this.stopLoading()
             }
