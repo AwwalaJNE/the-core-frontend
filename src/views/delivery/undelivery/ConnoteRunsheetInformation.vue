4b73d0424095ed43d47ac58bc2b207375cadc11e
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-11-13 12:25:34 +0700

    fix: link runsheet number in hrs page

--- src/views/delivery/undelivery/ConnoteRunsheetInformation.vue
+++ src/views/delivery/undelivery/ConnoteRunsheetInformation.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <table-master 
-            hideColumnKey="undelivery-info" 
-            :dataTable="dataTable" 
-            :dataColumn="datacolumn" 
+        <table-master
+            hideColumnKey="undelivery-info"
+            :dataTable="dataTable"
+            :dataColumn="datacolumn"
             :tableLoading="listenLoading"
             :pageSize="pagination.page_size"
             :page="pagination.page"
@@ -18,11 +18,11 @@
     </div>
 </template>
 <script>
-import axios from "axios";
-import master from "@/mixins/master"
-import TableMaster from "@/components/table/tableMaster.vue"
+import axios from 'axios'
+import master from '@/mixins/master'
+import TableMaster from '@/components/table/tableMaster.vue'
 export default {
-    name:"Inbound-Incoming",
+    name: 'Inbound-Incoming',
     mixins: [master],
     props: {
         query: String,
@@ -30,104 +30,112 @@ export default {
         loadingScan: Boolean,
     },
     components: {
-        "table-master" : TableMaster
+        'table-master': TableMaster,
     },
     data() {
         return {
             dataTable: [],
             datacolumn: [
                 {
-                    label: "Connote / Koli",
-                    key: "koli_number",
-                    width: "md"
+                    label: 'Connote / Koli',
+                    key: 'koli_number',
+                    width: 'md',
                 },
                 {
-                    label: "Delivery Runsheet Number",
-                    key: "delivery_runsheet_number",
-                    width: "auto"
+                    label: 'Delivery Runsheet Number',
+                    key: 'delivery_runsheet_number',
+                    width: 'auto',
                 },
                 {
-                    label: "Service",
-                    key: "connote_service_code",
-                    width: "xxs"
+                    label: 'Service',
+                    key: 'connote_service_code',
+                    width: 'xxs',
                 },
                 {
-                    label: "COD (Rp)",
-                    key: "amount_cod",
-                    width: "xs",
+                    label: 'COD (Rp)',
+                    key: 'amount_cod',
+                    width: 'xs',
                     type_amount: true,
-                    textAlign: "right"
+                    textAlign: 'right',
                 },
                 {
-                    label: "Status",
-                    key: "status",
-                    width: "xxs"
+                    label: 'Status',
+                    key: 'status',
+                    width: 'xxs',
                 },
                 {
-                    label: "Status Delivery",
-                    key: "status_delivery",
-                    width: "xxs"
+                    label: 'Status Delivery',
+                    key: 'status_delivery',
+                    width: 'xxs',
                 },
                 {
-                    label: "HRS",
-                    key: "hrs_value",
-                    type: "status",
-                    width: "xxs"
-                }
+                    label: 'HRS',
+                    key: 'hrs_value',
+                    type: 'status',
+                    width: 'xxs',
+                },
             ],
             loading: false,
             pagination: {
                 limit: 20,
                 page_size: 1,
-                page: 1
-            }
+                page: 1,
+            },
         }
     },
     computed: {
         listenEmployeeId() {
-            return this.employeeId;
+            return this.employeeId
         },
         listenLoading() {
-            return this.loadingScan || this.loading;
-        }
+            return this.loadingScan || this.loading
+        },
     },
     methods: {
         async getTableData(limit, page) {
             this.loading = true
             try {
-                const res = await axios.get(`${this.URL.courier_delivery}/${this.listenEmployeeId}/runsheet?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}`, this.Helper.header());
-                
+                const res = await axios.get(
+                    `${this.URL.courier_delivery}/${this.listenEmployeeId}/runsheet?n=${this.listenNodeId}&sort_order=desc&limit=${limit}&page=${page}`,
+                    this.Helper.header()
+                )
+
                 if (res.data.data.length > 0) {
                     let arr = res.data.data
                     arr.map((item, index) => {
-                        item["no"] = index + 1;
-                        item["koli_number"] = item.koli_number;
-                        item["delivery_runsheet_number"] = item.delivery_runsheet_number
-                        item["connote_service_code"] = item.connote_service_code
-                        item["amount_cod"] = item.amount_cod
-                        item["status"] = item.status
-                        item["status_delivery"] = item.status_delivery
+                        item['no'] = index + 1
+                        item['koli_number'] = item.koli_number
+                        item['delivery_runsheet_number'] = item.delivery_runsheet_number
+                        item['connote_service_code'] = item.connote_service_code
+                        item['amount_cod'] = item.amount_cod
+                        item['status'] = item.status
+                        item['status_delivery'] = item.status_delivery
                         item['hrs_value'] = item['is_hrs'] ? true : false
                         item['created_at'] = item.created_at
                     })
-                    this.dataTable = arr;
+                    this.dataTable = arr
 
-                    this.pagination.page = res.data.meta.current_page;
-                    this.pagination.limit = parseInt(res.data.meta.per_page);
-                    this.pagination.page_size = res.data.meta.last_page;
+                    this.pagination.page = res.data.meta.current_page
+                    this.pagination.limit = parseInt(res.data.meta.per_page)
+                    this.pagination.page_size = res.data.meta.last_page
                 } else {
                     this.dataTable = []
                 }
             } catch (err) {
-                this.openNotification("danger", err?.response?.data?.code ?? '', "Failed", err?.response?.data?.message ?? 'Something went wrong');
+                this.openNotification(
+                    'danger',
+                    err?.response?.data?.code ?? '',
+                    'Failed',
+                    err?.response?.data?.message ?? 'Something went wrong'
+                )
             } finally {
-                this.loading = false;
+                this.loading = false
             }
         },
-        closeDialogConfirm(){
+        closeDialogConfirm() {
             this.confirmDialog = false
         },
-        actionLimit(val){
+        actionLimit(val) {
             this.pagination.limit = val
             this.pagination.page = 1
             this.refresh()
@@ -136,15 +144,16 @@ export default {
             this.pagination.page = val
             this.refresh()
         },
-        refresh(){
-            this.getTableData(this.pagination.limit,this.pagination.page,this.tempSearch)
+        refresh() {
+            this.getTableData(this.pagination.limit, this.pagination.page, this.tempSearch)
         },
         showData(row) {
-            this.$router.push(`/delivery/runsheet/${this.listenEmployeeId}/edit/${row.delivery_runsheet_number}/${row.created_at.split(' ')[0]}`);
-            this.setRoutePageHistory(this.$route.meta, false);
+            this.$router.push(
+                `/delivery/runsheet/${this.listenEmployeeId}/edit/${row.delivery_runsheet_number}`
+            )
+            this.setRoutePageHistory(this.$route.meta, false)
         },
     },
-    mounted() {
-    }
+    mounted() {},
 }
 </script>
