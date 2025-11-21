2076888a3d21fe8ba2b5cd551b10dee08189c178
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-11-21 16:19:27 +0700

    fix: styling code for select

--- src/components/input/select.vue
+++ src/components/input/select.vue
@@ -28,34 +28,37 @@
                         
                     </vs-select> -->
                     <template v-if="listenHiddenTitle == false">
-                        <span class="c-label">{{ name }}<span v-if="rules && rules.includes('required')"> *</span></span>
+                        <span class="c-label"
+                            >{{ name
+                            }}<span v-if="rules && rules.includes('required')"> *</span></span
+                        >
                     </template>
-                    
-                    
+
                     <template v-if="listenIsMultiple == true">
-                      <el-select 
-                      ref="filterInputRef"
-                      v-model="arrValue" 
-                      filterable
-                      multiple
-                      :collapse-tags="listenIsCollapseTags"
-                      class="m-select"
-                      :placeholder="placeholder"
-                      :disabled="listenIsDisabled"
-                      :loading="loadingActive"
-                      @change="updateValue"
-                      @visible-change="setupSanitizeFilterInput"
-                      :data-testid="`select-${formKey}`"
-                      :state="props.err !== undefined && props.err !== '' ?'danger':'gray'">
-                          <el-option
-                          v-for="(item,key) in DataArr"
-                          :key="key"
-                          :label="item.label"
-                          :value="item.value"
-                          v-bind="getCustomAttribute(item.label)"
-                          >
-                          </el-option>
-                      </el-select>
+                        <el-select
+                            ref="filterInputRef"
+                            v-model="arrValue"
+                            filterable
+                            multiple
+                            :collapse-tags="listenIsCollapseTags"
+                            class="m-select"
+                            :placeholder="placeholder"
+                            :disabled="listenIsDisabled"
+                            :loading="loadingActive"
+                            @change="updateValue"
+                            @visible-change="setupSanitizeFilterInput"
+                            :data-testid="`select-${formKey}`"
+                            :state="props.err !== undefined && props.err !== '' ? 'danger' : 'gray'"
+                        >
+                            <el-option
+                                v-for="(item, key) in DataArr"
+                                :key="key"
+                                :label="item.label"
+                                :value="item.value"
+                                v-bind="getCustomAttribute(item.label)"
+                            >
+                            </el-option>
+                        </el-select>
                     </template>
                     <template v-else-if="listenIsMultipleTags == true">
                         <el-select
@@ -74,52 +77,56 @@
                             @change="updateValue"
                             @visible-change="setupSanitizeFilterInput"
                             :data-testid="`select-${formKey}`"
-                            :state="props.err !== undefined && props.err !== '' ?'danger':'gray'"
+                            :state="props.err !== undefined && props.err !== '' ? 'danger' : 'gray'"
                         >
                             <el-option
-                                v-for="(item,key) in DataArr"
+                                v-for="(item, key) in DataArr"
                                 :key="key"
                                 :label="item.label"
                                 :value="item.value"
-                                v-bind="getCustomAttribute(item.label)">
+                                v-bind="getCustomAttribute(item.label)"
+                            >
                             </el-option>
                         </el-select>
                     </template>
                     <template v-else-if="listenIsMultiple == false">
-                     <el-select
-                      ref="filterInputRef"
-                      v-model="value" 
-                      :allow-create="listenAllowCreate"
-                      filterable
-                      class="m-select"
-                      :placeholder="placeholder"
-                      :disabled="listenIsDisabled"
-                      @change="updateValue"
-                      @focus="inputFocus"
-                      @visible-change="setupSanitizeFilterInput"
-                      :clearable="listenHasClearButton"
-                      :loading="loadingActive"
-                      :data-testid="`select-${formKey}`"
-                      :state="props.err !== undefined && props.err !== '' ?'danger':'gray'">
+                        <el-select
+                            ref="filterInputRef"
+                            v-model="value"
+                            :allow-create="listenAllowCreate"
+                            filterable
+                            class="m-select"
+                            :placeholder="placeholder"
+                            :disabled="listenIsDisabled"
+                            @change="updateValue"
+                            @focus="inputFocus"
+                            @visible-change="setupSanitizeFilterInput"
+                            :clearable="listenHasClearButton"
+                            :loading="loadingActive"
+                            :data-testid="`select-${formKey}`"
+                            :state="props.err !== undefined && props.err !== '' ? 'danger' : 'gray'"
+                        >
                             <el-option
                                 v-for="(item, key) in DataArr"
                                 :key="key"
                                 :value="item.value"
-                                :label="!item.hasOwnProperty('formattedLabel') && (item.code || item.label)"
+                                :label="
+                                    !item.hasOwnProperty('formattedLabel') &&
+                                    (item.code || item.label)
+                                "
                             >
                                 <template v-if="item.hasOwnProperty('formattedLabel')">
-                                <span v-for="(line, index) in item.formattedLabel" :key="index">
-                                    {{ line }}
-                                    <br v-if="index < item.formattedLabel.length - 1" />
-                                </span>
+                                    <span v-for="(line, index) in item.formattedLabel" :key="index">
+                                        {{ line }}
+                                        <br v-if="index < item.formattedLabel.length - 1" />
+                                    </span>
                                 </template>
                                 <template v-else>
-                                {{ item.label }}
+                                    {{ item.label }}
                                 </template>
                             </el-option>
-                      </el-select>
+                        </el-select>
                     </template>
-                    
                 </template>
             </inputan>
         </template>
@@ -127,12 +134,12 @@
     </div>
 </template>
 <script>
-import Inputan from "@/components/input/inputan"
+import Inputan from '@/components/input/inputan'
 export default {
-    name:"select-filter",
+    name: 'select-filter',
     components: {
-        "inputan": Inputan
-    }, 
+        inputan: Inputan,
+    },
     props: {
         name: String,
         rules: String,
@@ -145,7 +152,7 @@ export default {
         isMultiple: Boolean,
         isMultipleTag: Boolean,
         border: Boolean,
-        placeholder:String,
+        placeholder: String,
         tabindex: [Number, String],
         disabled: Boolean,
         hiddenTitle: Boolean,
@@ -156,13 +163,15 @@ export default {
     },
     data() {
         return {
-            DataArr: this.valueData ? this.valueData : [
-                {
-                    label: 'No Data',
-                    value: 'nodata'
-                }
-            ],
-            value: this.selectedValue ? this.selectedValue :"",
+            DataArr: this.valueData
+                ? this.valueData
+                : [
+                      {
+                          label: 'No Data',
+                          value: 'nodata',
+                      },
+                  ],
+            value: this.selectedValue ? this.selectedValue : '',
             arrValue: this.selectedValue ? this.selectedValue : [],
             loadingActive: false,
             // loading: true,
@@ -170,13 +179,13 @@ export default {
         }
     },
     computed: {
-        listenFormKey(){
+        listenFormKey() {
             return this.formKey || ''
         },
-        listenIsMultiple(){
+        listenIsMultiple() {
             return this.isMultiple ? this.isMultiple : false
         },
-        listenIsMultipleTags(){
+        listenIsMultipleTags() {
             return this.isMultipleTag ? this.isMultipleTag : false
         },
         listenTabIndex() {
@@ -195,8 +204,8 @@ export default {
             return this.isAllowCreate ? this.isAllowCreate : false
         },
         listenHasClearButton() {
-            return this.hasClearButton || false;
-        }
+            return this.hasClearButton || false
+        },
     },
     watch: {
         valueData: function (val) {
@@ -207,20 +216,19 @@ export default {
         },
         selectedValue: function (val) {
             if (val != undefined) {
-                if(this.isMultiple == false && this.listenIsMultipleTags == false) {
+                if (this.isMultiple == false && this.listenIsMultipleTags == false) {
                     this.value = val
-                } else if(this.listenIsMultipleTags == true) {
-                    this.arrValue = val;
+                } else if (this.listenIsMultipleTags == true) {
+                    this.arrValue = val
                 } else {
                     this.arrValue = val
                 }
             }
         },
-        loading: function(val) {
-          if(val !== undefined) {
-            this.loadingActive = val || false
-          }
-
+        loading: function (val) {
+            if (val !== undefined) {
+                this.loadingActive = val || false
+            }
         },
         // loadingData: function (val) {
         //     if(val != undefined) {
@@ -242,87 +250,92 @@ export default {
         // closeLoading(){
         //     this.loadingInjector !== null ? this.loadingInjector.close() : null
         // },
-        updateValue(val){
-            let dataValue = this.listenIsMultiple == false && this.isMultipleTag === false ? this.value : this.arrValue
-            let obj = this.DataArr.filter(item => item.value == val)[0]
-            this.$emit("updateValue", this.listenFormKey, dataValue, obj, this.dataObj)
+        updateValue(val) {
+            let dataValue =
+                this.listenIsMultiple == false && this.isMultipleTag === false
+                    ? this.value
+                    : this.arrValue
+            let obj = this.DataArr.filter((item) => item.value == val)[0]
+            this.$emit('updateValue', this.listenFormKey, dataValue, obj, this.dataObj)
         },
         inputFocus() {
-            this.$emit("inputFocus", this.dataObj)
+            this.$emit('inputFocus', this.dataObj)
         },
         getCustomAttribute(label) {
             return {
-                [this.customBind]: label
-            };
+                [this.customBind]: label,
+            }
         },
         setupSanitizeFilterInput() {
             this.$nextTick(() => {
-                const inputEls = this.$el.querySelectorAll('input.el-input__inner, input.el-select__input');
+                const inputEls = this.$el.querySelectorAll(
+                    'input.el-input__inner, input.el-select__input'
+                )
 
-                inputEls.forEach(input => {
-                    if (input._hasSanitizeListener) return;
+                inputEls.forEach((input) => {
+                    if (input._hasSanitizeListener) return
 
-                    input.addEventListener('input', e => {
-                        const clean = e.target.value.replace(/[^a-zA-Z0-9_\-\*\(\)~ ,\/]/g, '');
+                    input.addEventListener('input', (e) => {
+                        const clean = e.target.value.replace(/[^a-zA-Z0-9_\-\*\(\)~ ,\/]/g, '')
                         if (e.target.value !== clean) {
-                        e.target.value = clean;
-                        e.target.dispatchEvent(new Event('input'));
+                            e.target.value = clean
+                            e.target.dispatchEvent(new Event('input'))
                         }
-                    });
+                    })
 
-                    input._hasSanitizeListener = true;
-                });
-            });
-        }
+                    input._hasSanitizeListener = true
+                })
+            })
+        },
     },
     mounted() {
-        this.setupSanitizeFilterInput();
+        this.setupSanitizeFilterInput()
     },
 }
 </script>
 <style lang="scss" scoped>
-    .m-select{
-        &.vs-select-content{
-            max-width: unset;
-            margin: 10px 0;
-        }
-        .vs-select__label--label{
-            transform: translate(-3px, -28px) !important;
-        }
-        .vs-select.activeOptions .vs-select__input:focus ~ .vs-select__label--label {
-            transform: translate(-3%, -28px) !important;
-        }
-        &.el-select .el-input .el-input__inner{
-            margin-bottom: 0;
-        }
+.m-select {
+    &.vs-select-content {
+        max-width: unset;
+        margin: 10px 0;
+    }
+    .vs-select__label--label {
+        transform: translate(-3px, -28px) !important;
+    }
+    .vs-select.activeOptions .vs-select__input:focus ~ .vs-select__label--label {
+        transform: translate(-3%, -28px) !important;
+    }
+    &.el-select .el-input .el-input__inner {
+        margin-bottom: 0;
+    }
 
-        &.el-input{
-            &.el-input--suffix{
-                .el-input__suffix{
-                    .el-input__suffix-inner{
-                        position: relative;
-                        top: 8px;
-                    }
+    &.el-input {
+        &.el-input--suffix {
+            .el-input__suffix {
+                .el-input__suffix-inner {
+                    position: relative;
+                    top: 8px;
                 }
             }
         }
-        .el-input__icon{
-            height: auto;
-        }
     }
-    .c-label{
-            font-size: 0.75rem;
-            /* left: 0px; */
-            position: relative;
-            align-content: start;
-            display: block;
-            padding: 4px 7px;
-            text-align: left;
-        }
+    .el-input__icon {
+        height: auto;
+    }
+}
+.c-label {
+    font-size: 0.75rem;
+    /* left: 0px; */
+    position: relative;
+    align-content: start;
+    display: block;
+    padding: 4px 7px;
+    text-align: left;
+}
 </style>
 <style scoped>
-    .el-select-dropdown__item {
-        min-height: 34px;
-        height: fit-content !important;
-    }
+.el-select-dropdown__item {
+    min-height: 34px;
+    height: fit-content !important;
+}
 </style>
