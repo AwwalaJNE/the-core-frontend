677dfe40c579a9677667fcd15741d9b668a4ff04
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2025-12-18 12:21:38 +0700

    fix: change value into boolean

--- src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
+++ src/views/transport/suratJalanNew/dialogCreateSuratJalanV2.vue
@@ -404,7 +404,7 @@ export default {
                 manifest_lov: val.manifest_lov,
                 item_no: val.item_number,
                 is_penerusan: val.is_penerusan,
-                auto_depart: val.auto_depart,
+                auto_depart: val?.auto_depart === 1 ? 1 : 0,
             }
         },
         async getEditDataByApi() {
@@ -495,7 +495,7 @@ export default {
                 manifest_lov: val.manifest_lov,
                 item_no: val.item_number,
                 is_penerusan: val.is_penerusan,
-                auto_depart: val.auto_depart,
+                auto_depart: val?.auto_depart === 1 ? 1 : 0,
             }
         },
         onChangeCustom(type, val, obj) {
@@ -508,7 +508,7 @@ export default {
 
             switch (type) {
                 case 'auto_depart':
-                    updateMasterForm('auto_depart', val ? '1' : '0')
+                    updateMasterForm('auto_depart', val ? 1 : 0)
                     break
                 case 'destination_id':
                     updateMasterForm('node_id_destination', val)
@@ -618,7 +618,7 @@ export default {
                         manifest_lov: data.manifest_lov,
                         item_no: data.item_number,
                         is_penerusan: data.is_penerusan,
-                        auto_depart: data.auto_depart,
+                        auto_depart: data?.auto_depart === 1 ? 1 : 0,
                     }
                     this.editData = {
                         manifest_do_number: data.manifest_do_number,
