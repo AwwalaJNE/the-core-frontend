a4199a0011711b103acc39486d18ff94e90b372e
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2026-01-05 12:44:45 +0700

    fix: remove unused item

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -3,15 +3,17 @@
         <vs-col xs="12" sm="12" lg="12">
             <div class="map_picker">
                 <div class="query_map">
-                    <!-- <div>Search desired location</div>
+                    <!-- Search desired location (currently disabled) -->
+                    <!--
                     <el-autocomplete
-                    class="inline-input"
-                    v-model="locationName"
-                    :fetch-suggestions="querySearch"
-                    placeholder="Please Input"
-                    :trigger-on-focus="false"
-                    @select="handleSelect"
-                    ></el-autocomplete> -->
+                        class="inline-input"
+                        v-model="locationName"
+                        :fetch-suggestions="querySearch"
+                        placeholder="Please Input"
+                        :trigger-on-focus="false"
+                        @select="handleSelect"
+                    />
+                    -->
                 </div>
 
                 <div
@@ -30,15 +32,17 @@
         </vs-col>
     </vs-row>
 </template>
+
 <script>
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-
 import axios from 'axios'
 import master from '@/mixins/master'
+
 export default {
     name: 'map-component',
     mixins: [master],
+
     props: {
         title: String,
         lat: Number,
@@ -49,6 +53,7 @@ export default {
         dataLtnLng: String,
         summonMap: Boolean,
     },
+
     data() {
         return {
             map: {},
@@ -60,6 +65,7 @@ export default {
             locationName: '',
         }
     },
+
     watch: {
         lat(val) {
             this.latitude = val
@@ -74,38 +80,23 @@ export default {
             this.getLocation()
         },
         summonMap(val) {},
-        // dataLtnLng(val) {
-        //     try {
-        //         if(val){
-        //             let temp = val.split(",");
-        //             this.latitude = temp[0];
-        //             this.longitude = temp[1];
-        //         }
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // },
     },
+
     methods: {
         setMarker() {
             let curLocation = [this.latitude, this.longitude]
 
-            let self = this
-            if (curLocation[0] == 0 && curLocation[1] == 0) {
+            if (curLocation[0] === 0 && curLocation[1] === 0) {
                 curLocation = [-6.21462, 106.84513]
             }
 
-            // document.getElementById("map_general").innerHTML =
-            // "<div id='map' style='width: 100%; height: 100%;'></div>";
             this.$refs.map_general.innerHTML =
                 "<div id='map' style='width: 100%; height: 100%;'></div>"
 
             const container = L.DomUtil.get('map')
-            if (container != null) {
-                container._leaflet_id = null
-            }
+            if (container) container._leaflet_id = null
 
-            var map = new L.Map('map').setView(curLocation, this.zoom)
+            const map = new L.Map('map').setView(curLocation, this.zoom)
 
             L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 attribution:
@@ -114,29 +105,28 @@ export default {
 
             map.attributionControl.setPrefix(false)
 
-            var marker = new L.marker(curLocation, {
+            const marker = new L.marker(curLocation, {
                 draggable: 'true',
             })
 
-            marker.on('dragend', function (event) {
-                var position = marker.getLatLng()
-                marker
-                    .setLatLng(position, {
-                        draggable: 'true',
-                    })
-                    .bindPopup(position)
-                    .update()
-                self.latitude = position.lat
-                self.longitude = position.lng
-                self.getCoordinates()
+            marker.on('dragend', () => {
+                const position = marker.getLatLng()
 
-                self.emitThem()
+                marker.setLatLng(position, { draggable: 'true' }).bindPopup(position).update()
+
+                this.latitude = position.lat
+                this.longitude = position.lng
+
+                this.getCoordinates()
+                this.emitThem()
             })
 
             map.addLayer(marker)
+
             this.marker = marker
             this.map = map
         },
+
         async querySearch(queryString, cb) {
             if (!queryString) {
                 cb([])
@@ -151,49 +141,31 @@ export default {
 
                 const items = res?.data?.items ?? []
 
-                const suggestions = items.map((item) => ({
-                    value: item.address.label,
-                    data: item,
-                }))
-
-                cb(suggestions)
+                cb(
+                    items.map((item) => ({
+                        value: item.address.label,
+                        data: item,
+                    }))
+                )
             } catch (error) {
                 console.error('Geocode error:', error)
                 cb([])
             }
         },
-        // getCurrentLocation() {
-        // ISSUE diblock sama browser
 
-        //     navigator.geolocation.getCurrentPosition(
-        //         position => {
-        //         this.center = {
-        //             lat: parseFloat(position.coords.latitude),
-        //             lng: parseFloat(position.coords.longitude)
-        //         };
-        //         this.latitude = position.coords.latitude;
-        //         this.longitude = position.coords.longitude;
-        //         },
-        //         failure => {
-        //             console.log(failure, "Failure");
-        //             if (failure.message.startsWith("Only secure origins are allowed")) {
-        //                 this.openNotification('danger', err.response ? err.response.data.code : '', 'Failed get current location', 'Only secure origins are allowed')
-        //             }
-        //         }
-        //     );
-        // },
         getLocation() {
-            navigator.geolocation.getCurrentPosition((position) => {
+            navigator.geolocation.getCurrentPosition(() => {
                 this.center = {
                     lat: parseFloat(this.lat),
                     lng: parseFloat(this.lng),
                 }
             })
         },
+
         async getCoordinates() {
             try {
                 const res = await axios.get(
-                    `${this.URL.reverse_geocode}?n=${this.listenNodeId}lat=${this.latitude}&long=${this.longitude}`,
+                    `${this.URL.reverse_geocode}?n=${this.listenNodeId}&lat=${this.latitude}&long=${this.longitude}`,
                     this.Helper.header()
                 )
 
@@ -203,50 +175,49 @@ export default {
                 console.error('error', error)
             }
         },
+
         handleSelect(item) {
-            let self = this
-            let selected = item.data
-            let marker = {
+            const selected = item.data
+            const marker = {
                 lat: selected.position.lat,
                 lng: selected.position.lng,
             }
-            self.latitude = marker.lat
-            self.longitude = marker.lng
-            self.locationName = selected.address.label
-            // self.locationName = 'Berlin'
-            self.center = marker
 
-            self.marker.setLatLng([marker.lat, marker.lng])
-            self.map.panTo([marker.lat, marker.lng], self.zoom)
+            this.latitude = marker.lat
+            this.longitude = marker.lng
+            this.locationName = selected.address.label
+            this.center = marker
 
-            // self.emitThem()
+            this.marker.setLatLng([marker.lat, marker.lng])
+            this.map.panTo([marker.lat, marker.lng], this.zoom)
         },
+
         emitThem() {
-            let dataMap = {
+            this.$emit('pickLocation', {
                 location: this.locationName,
                 latitude: this.latitude,
                 longitude: this.longitude,
-                lat_lng: this.latitude + ',' + this.longitude,
+                lat_lng: `${this.latitude},${this.longitude}`,
                 parameterMap: this.parameterMap,
-            }
-            this.$emit('pickLocation', dataMap)
+            })
         },
     },
-    mounted() {
-        let self = this
-        setTimeout(function () {
-            self.latitude = self.lat
-            self.longitude = self.lon
 
-            self.getCoordinates()
-            self.$nextTick((_) => {
-                self.setMarker()
-                // self.getCurrentLocation()
+    mounted() {
+        setTimeout(() => {
+            this.latitude = this.lat
+            this.longitude = this.lon
+
+            this.getCoordinates()
+
+            this.$nextTick(() => {
+                this.setMarker()
             })
-        }, 400) // 4 mili detik | fix issue prop lat dan lon masih 0
+        }, 400)
     },
 }
 </script>
+
 <style lang="scss">
 .query_map {
     position: relative;
@@ -254,6 +225,7 @@ export default {
     text-align: left;
     font-size: 0.75rem;
 }
+
 .map_picker {
     position: relative;
     widows: 100%;
