54916e5ab25c68c9195a599ad4bb38f5b50cc2f4
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2026-01-08 14:57:31 +0700

    feat: add is approve info

--- src/views/delivery/runsheetNew/runsheetTable.vue
+++ src/views/delivery/runsheetNew/runsheetTable.vue
@@ -210,6 +210,7 @@ export default {
                         let total_delivered = []
                         let total_undelivered = []
                         let total_undelivery_received = []
+                        let is_approve = []
 
                         item['total_runsheet'] = item['total_runsheet'] || '0'
                         item['total_hrs'] = item['total_hrs'] || '0'
@@ -220,19 +221,6 @@ export default {
                         item['total_undelivery_received'] =
                             item['total_undelivered_received'] || '0'
 
-                        item['children_width'] = {
-                            Priority: 'auto',
-                            Id: 'md',
-                            'DRI Number': 'sm',
-                            'HRS Number': 'sm',
-                            'Total Koli': 'auto',
-                            Open: 'xxxs',
-                            Status: 'xxxs',
-                            Delivered: 'xxxs',
-                            Undelivered: 'xxxs',
-                            'Undelivered Received': 'xs',
-                            HRS: 'xxxxs',
-                        }
                         item['children_hide_label'] = ['Priority']
                         item['children_type'] = {
                             Priority: 'icon-warning',
@@ -241,6 +229,7 @@ export default {
                             Priority: 'Contains Undelivered Koli',
                         }
                         item.delivery.map((el) => {
+                            is_approve.push(el.is_approve ? true : false)
                             has_undelivered_connote.push(el.has_undelivered_connote)
                             delivery_runsheet_number.push(el.delivery_runsheet_number)
                             dri.push(el.dri ?? '-')
@@ -253,6 +242,7 @@ export default {
                             total_undelivery_received.push(el.total_undelivery_received)
                         })
                         children['Priority'] = has_undelivered_connote
+                        children['Is Approve'] = is_approve
                         children['Id'] = delivery_runsheet_number
                         children['DRI Number'] = dri
                         children['HRS Number'] = hrs
