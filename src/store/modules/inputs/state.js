e4a33c7889d8e0398d002ea56afca23463725fbb
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2026-01-30 11:17:53 +0700

    fix: type for phone numeric in pickup

--- src/store/modules/inputs/state.js
+++ src/store/modules/inputs/state.js
@@ -4068,11 +4068,11 @@ export default {
         pickup_phone_number: {
             label: 'Phone',
             key: 'pickup_phone_number',
-            rule: 'required|phone',
+            rule: 'required|phone|numeric',
             typeInput: 'text',
             typeData: 'String',
             valueData: null,
-            value: '',
+            value: '62',
         },
         pickup_address: {
             label: 'Address',
