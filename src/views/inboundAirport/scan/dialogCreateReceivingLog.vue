be3b7449e9e956b23e9ae5b1334c7b0d10bcc90c
AwwalaJNE <148318355+AwwalaJNE@users.noreply.github.com>
2026-01-31 19:33:36 +0700

    fix: header form data for receiving log

--- src/views/inboundAirport/scan/dialogCreateReceivingLog.vue
+++ src/views/inboundAirport/scan/dialogCreateReceivingLog.vue
@@ -325,6 +325,7 @@ export default {
                     ? `${this.URL.receiving_log}/${this.receivingLogId}?n=${this.listenNodeId}`
                     : `${this.URL.receiving_log}?n=${this.listenNodeId}`
                 const res = await axios.post(`${endpoint}`, formData, this.Helper.header())
+                const res = await axios.post(`${endpoint}`, formData, this.Helper.headerFormData())
                 if (!this.receivingLogId) {
                     const resData = res.data.data
                     this.receivingLogId = resData.receiving_log_id
